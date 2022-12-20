4380575f71d3d5e047cc1ac39b80439ac92a4e1d
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2022-12-20 22:29:17 +0100

    initial commit

---
+++ Java/E-montazysta/src/main/java/com/emontazysta/EMontazystaApplication.java
@@ -0,0 +1,13 @@
+package com.emontazysta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMontazystaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EMontazystaApplication.class, args);
+    }
+
+}
---
+++ Java/E-montazysta/src/test/java/com/emontazysta/EMontazystaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emontazysta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMontazystaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
