8b9cbaafaf023f8d83258a4a1070c3f3084736e7
Kamil Kuryłek <kurylek98@gmail.com>
2022-12-25 00:02:42 +0100

    Update Location.java - Add @Entity

--- Java/E-montazysta/src/main/java/com/emontazysta/model/Location.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/model/Location.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
