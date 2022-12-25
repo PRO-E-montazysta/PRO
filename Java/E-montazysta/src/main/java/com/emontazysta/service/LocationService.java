de296fc898a0d3700b32841f469c71abf26ed573
Kamil Kuryłek <kurylek98@gmail.com>
2022-12-25 01:29:43 +0100

    Create Repository and Service for Location

---
+++ Java/E-montazysta/src/main/java/com/emontazysta/service/LocationService.java
@@ -0,0 +1,37 @@
+package com.emontazysta.service;
+
+import com.emontazysta.model.Location;
+import com.emontazysta.repositoriy.LocationRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class LocationService {
+
+    private final LocationRepository locationRepository;
+
+    public List<Location> getLocations() {
+        return locationRepository.findAll();
+    }
+
+    public Location getLocation(Long id) {
+        return locationRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Location with id " + id + " not found!"));
+    }
+
+    public void addLocation(Location request) {
+        locationRepository.save(request);
+    }
+
+    public void deleteLocation(Long id) {
+        locationRepository.deleteById(id);
+    }
+
+    public void updateLocation(Long id, Location updatedLocation) {
+        updatedLocation.setId(id);
+        locationRepository.save(updatedLocation);
+    }
+}
