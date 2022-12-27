2e038115caa17aa5c6f1a663cd94eaf0ca1adfb0
Kamil Kuryłek <kurylek98@gmail.com>
2022-12-27 21:06:53 +0100

    Create CompanyService.java

---
+++ Java/E-montazysta/src/main/java/com/emontazysta/service/CompanyService.java
@@ -0,0 +1,45 @@
+package com.emontazysta.service;
+
+import com.emontazysta.data.CompanyRequest;
+import com.emontazysta.model.Company;
+import com.emontazysta.repositoriy.CompanyRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CompanyService {
+
+    private final CompanyRepository companyRepository;
+
+    public List<Company> getCompanies() {
+        return companyRepository.findAll();
+    }
+
+    public Company getCompany(Long id) {
+        return companyRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Company with id " + id + " not found!"));
+    }
+
+    public void addCompany(CompanyRequest companyToAdd) {
+        Company newCompany = new Company();
+        newCompany.setCompanyName(companyToAdd.getCompanyName());
+        newCompany.setCreatedAt(new Date());
+
+        companyRepository.save(newCompany);
+    }
+
+    public void deleteCompany(Long id) {
+        companyRepository.deleteById(id);
+    }
+
+    public void updateCompany(Long id, CompanyRequest companyToAdd) {
+        Company updatedCompany = getCompany(id);
+        updatedCompany.setCompanyName(companyToAdd.getCompanyName());
+
+        companyRepository.save(updatedCompany);
+    }
+}
