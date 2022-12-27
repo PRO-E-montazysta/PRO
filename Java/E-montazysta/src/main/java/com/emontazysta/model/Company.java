ccb45b8c8361c6cdc37949efe92eef5bc13319c6
Kamil Kuryłek <kurylek98@gmail.com>
2022-12-27 20:34:35 +0100

    Create Company.java

---
+++ Java/E-montazysta/src/main/java/com/emontazysta/model/Company.java
@@ -0,0 +1,26 @@
+package com.emontazysta.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String companyName;
+
+    private Date createdAt;
+}
