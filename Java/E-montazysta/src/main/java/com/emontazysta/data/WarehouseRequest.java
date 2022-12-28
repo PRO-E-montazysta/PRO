dd18e67cb1b5543b74239fe977a6e51568bec3cf
Kamil Kuryłek <kurylek98@gmail.com>
2022-12-28 21:24:24 +0100

    Create WarehouseRequest.java

---
+++ Java/E-montazysta/src/main/java/com/emontazysta/data/WarehouseRequest.java
@@ -0,0 +1,13 @@
+package com.emontazysta.data;
+
+import lombok.Data;
+
+@Data
+public class WarehouseRequest {
+    
+    private String name;
+
+    private String description;
+
+    private String openingHours;
+}
