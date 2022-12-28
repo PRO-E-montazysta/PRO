3e019b1959b1ec45cad8587a5a8e29558ee081d3
Kamil Kuryłek <kurylek98@gmail.com>
2022-12-29 00:05:33 +0100

    Crate Repository and Service for  Unavailability

---
+++ Java/E-montazysta/src/main/java/com/emontazysta/repositoriy/UnavailabilityRepository.java
@@ -0,0 +1,7 @@
+package com.emontazysta.repositoriy;
+
+import com.emontazysta.model.Unavailability;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UnavailabilityRepository extends JpaRepository<Unavailability, Long> {
+}
---
+++ Java/E-montazysta/src/main/java/com/emontazysta/service/UnavailabilityService.java
@@ -0,0 +1,52 @@
+package com.emontazysta.service;
+
+import com.emontazysta.data.UnavailabilityRequest;
+import com.emontazysta.model.Unavailability;
+import com.emontazysta.repositoriy.UnavailabilityRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UnavailabilityService {
+
+    private final UnavailabilityRepository unavailabilityRepository;
+
+    public List<Unavailability> getUnavailabilities() {
+        return unavailabilityRepository.findAll();
+    }
+
+    public Unavailability getUnavailability(Long id) {
+        return unavailabilityRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Unavailability with id " + id + " not found!"));
+    }
+
+    public void addUnavailability(UnavailabilityRequest unavailabilityToAdd) {
+        Unavailability newUnavailability = new Unavailability();
+        newUnavailability.setTypeOfUnavailability(unavailabilityToAdd.getTypeOfUnavailability());
+        newUnavailability.setDescription(unavailabilityToAdd.getDescription());
+        newUnavailability.setUnavailableFrom(unavailabilityToAdd.getUnavailableFrom());
+        newUnavailability.setUnavailableTo(unavailabilityToAdd.getUnavailableTo());
+
+        unavailabilityRepository.save(newUnavailability);
+    }
+
+    public void deleteUnavailability(Long id) {
+        unavailabilityRepository.deleteById(id);
+    }
+
+    public void updateUnavailability(Long id, UnavailabilityRequest unavailabilityToUpdate) {
+        Unavailability updatedUnavailability = this.getUnavailability(id);
+        updatedUnavailability.setTypeOfUnavailability(unavailabilityToUpdate.getTypeOfUnavailability());
+        updatedUnavailability.setDescription(unavailabilityToUpdate.getDescription());
+        updatedUnavailability.setUnavailableFrom(unavailabilityToUpdate.getUnavailableFrom());
+        updatedUnavailability.setUnavailableTo(unavailabilityToUpdate.getUnavailableTo());
+
+
+        unavailabilityRepository.save(updatedUnavailability);
+    }
+}
