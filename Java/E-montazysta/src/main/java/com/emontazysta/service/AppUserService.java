c599bec2d68c6b2b04cbb18a5a8e9e30ae5dc13b
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2022-12-28 22:13:54 +0100

    dodane uwierzytelnianie i autoryzacja przy uzyciu tokena jwt podniesiona wersja Spring Boot

--- Java/E-montazysta/src/main/java/com/emontazysta/service/AppUserService.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/service/AppUserService.java
@@ -1,13 +1,63 @@
 package com.emontazysta.service;
 
-
+import com.emontazysta.Role;
 import com.emontazysta.model.AppUser;
+import com.emontazysta.repositoriy.AppUserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-public interface AppUserService {
-    AppUser saveUser(AppUser user);
-    void addRoleToUser(String username, String roleName);
-    AppUser getUser(String username);
-    List<AppUser> getUsers();
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class AppUserService implements UserDetailsService {
+
+    private final AppUserRepository appUserRepository;
+
+
+    public AppUser saveUser(AppUser user, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        log.info("Saving new user {} to the database",user.getUsername());
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        return appUserRepository.save(user);
+    }
+
+    public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} to user {}", roleName, username);
+        AppUser user = appUserRepository.findByUsername(username);
+        user.getRoles().add(Role.valueOf(roleName));
+
+    }
+
+    public AppUser getUser(String username) {
+        log.info("Fetching user {}", username);
+        return appUserRepository.findByUsername(username);
+    }
+
+    public List<AppUser> getUsers() {
+        log.info("Fetching all users");
+        return appUserRepository.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = appUserRepository
+                .findByUsername(username);
+        String password = user.getPassword();
+        if(user == null){
+            log.error("User not found in database");
+            throw new UsernameNotFoundException("User not found in database");
+        } else {
+            log.info("User found in the database: {}", username);
+            log.info("User found in the database: {}", password);
+        }
+        return user;
+    }
 }
