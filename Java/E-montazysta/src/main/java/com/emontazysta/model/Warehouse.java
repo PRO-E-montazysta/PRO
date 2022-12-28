bf7e7f2439a559ec8ae4a770ade233c201ee98a9
Kamil Kuryłek <kurylek98@gmail.com>
2022-12-28 21:23:43 +0100

    Create Warehouse.java

---
+++ Java/E-montazysta/src/main/java/com/emontazysta/model/Warehouse.java
@@ -0,0 +1,27 @@
+package com.emontazysta.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String openingHours;
+}
