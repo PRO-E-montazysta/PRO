572841a97b077fee105a23bb039481d9406e816f
Kamil Kuryłek <kurylek98@gmail.com>
2022-12-29 22:35:47 +0100

    Fix after Spring update

--- Java/E-montazysta/src/main/java/com/emontazysta/model/Unavailability.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/model/Unavailability.java
@@ -2,14 +2,14 @@ package com.emontazysta.model;
 
 import com.emontazysta.enums.TypeOfUnavailability;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.util.Date;
 
 @Entity
