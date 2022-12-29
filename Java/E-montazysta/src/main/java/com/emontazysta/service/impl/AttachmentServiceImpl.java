57b4d9442a394dcfa4f7294e270d14264bdcb0dd
Kamil Kuryłek <kurylek98@gmail.com>
2022-12-29 23:45:03 +0100

    Create AttachmentServiceImpl.java

---
+++ Java/E-montazysta/src/main/java/com/emontazysta/service/impl/AttachmentServiceImpl.java
@@ -0,0 +1,38 @@
+package com.emontazysta.service.impl;
+
+import com.emontazysta.model.Attachment;
+import com.emontazysta.repository.AttachmentRepository;
+import com.emontazysta.service.AttachmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AttachmentServiceImpl implements AttachmentService {
+
+    private final AttachmentRepository repository;
+
+    @Override
+    public List<Attachment> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Attachment getById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Override
+    public void add(Attachment attachment) {
+        repository.save(attachment);
+    }
+
+    @Override
+    public void delete(Long id) {
+    repository.deleteById(id);
+    }
+}
