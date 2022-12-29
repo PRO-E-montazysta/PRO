8407d615872689a1f2f8a3ec05afc39a8e62a6a8
Kryhowsky <piter71-1997@o2.pl>
2022-12-29 18:19:10 +0100

    Updated employee hierarchy, updated OpenAPI dependency

--- Java/E-montazysta/src/main/java/com/emontazysta/model/Employee.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/model/Employee.java
@@ -4,25 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @MappedSuperclass
-public abstract class Employee {
+public abstract class Employee extends AppUser {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String username;
-    private String password;
-    private String email;
     private String phone;
     private String pesel;
 }
