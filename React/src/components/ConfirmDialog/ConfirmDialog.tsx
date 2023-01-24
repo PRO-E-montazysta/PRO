faa5e07106c281512ce5419d9dcfe28f7e71e9da
Kamil Kuryłek <kurylek98@gmail.com>
2023-01-24 22:07:21 +0100

    Added buttons styles

--- React/src/components/ConfirmDialog/ConfirmDialog.tsx
+++ React/src/components/ConfirmDialog/ConfirmDialog.tsx
@@ -11,10 +11,32 @@ const ConfirmDialog = () => {
                     Czy na pewno chcesz usunąć konto użytkownika o ID 95259849?
                 </p>
                 <div className="action-buttons">
-                    <Button sx={{ color: 'white' }}>
+                    <Button sx={{
+                        color: 'white',
+                        background: '#282A3A',
+                        width: '100px',
+                        height: '40px',
+                        'border-radius': '3px',
+                        'font-weight': '600',
+                        'font-size': '13px',
+                        'line-height': '32px',
+                        'letter-spacing': '0.2px'
+                        }}
+                    >
                         Usuń
                     </Button>
-                    <Button sx={{ color: 'white' }}>
+                    <Button sx={{
+                        color: 'white',
+                        background: '#15171F',
+                        width: '100px',
+                        height: '40px',
+                        'border-radius': '3px',
+                        'font-weight': '600',
+                        'font-size': '13px',
+                        'line-height': '32px',
+                        'letter-spacing': '0.2px'
+                        }}
+                    >
                         Anuluj
                     </Button>
                 </div>
