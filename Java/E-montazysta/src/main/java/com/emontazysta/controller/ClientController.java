c9b194c1d01f3ed91f99693af55d6d10bccb0500
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2023-01-24 20:27:10 +0100

    Utworzenie dto oraz mapera dla modelu. Dodanie adnotacji walidacyjnych dla atrybutów klasy oraz kontrolera

--- Java/E-montazysta/src/main/java/com/emontazysta/controller/ClientController.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/controller/ClientController.java
@@ -1,15 +1,20 @@
 package com.emontazysta.controller;
 
+import com.emontazysta.mapper.ClientMapper;
 import com.emontazysta.model.Client;
+import com.emontazysta.model.dto.ClientDto;
 import com.emontazysta.service.impl.ClientServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.emontazysta.configuration.Constants.API_BASE_CONSTANT;
 
@@ -20,32 +25,34 @@ public class ClientController {
 
     private final ClientServiceImpl clientService;
 
-    @GetMapping
+    @GetMapping("/all")
     @Operation(description = "Allows to get all Clients.", security = @SecurityRequirement(name = "bearer-key"))
-    public List<Client> getAll() {
-        return clientService.getAll();
+    public ResponseEntity <List<ClientDto>> getAll() {
+        return ResponseEntity.ok().body(clientService.getAll().stream()
+                .map(ClientMapper::clientToDto)
+                .collect(Collectors.toList()));
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     @Operation(description = "Allows to get Client by given Id.", security = @SecurityRequirement(name = "bearer-key"))
-    public Client getById(@PathVariable Long id) {
-        return clientService.getById(id);
+    public ClientDto getById(@PathVariable Long id) {
+        return ClientMapper.clientToDto(clientService.getById(id));
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(description = "Allows to add new Client.", security = @SecurityRequirement(name = "bearer-key"))
-    public void add(@RequestBody Client client) {
+    public void add(@Valid @RequestBody Client client) {
         clientService.add(client);
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}")
     @Operation(description = "Allows to delete Client by given Id.", security = @SecurityRequirement(name = "bearer-key"))
     public void deleteById(@PathVariable Long id) {
         clientService.delete(id);
     }
 
-    @PutMapping("{id}")
+    @PutMapping("/{id}")
     @Operation(description = "Allows to update Client by given Id and Client.", security = @SecurityRequirement(name = "bearer-key"))
     public void update(@PathVariable Long id, @RequestBody Client client) {
         clientService.update(id, client);
