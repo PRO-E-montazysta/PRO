8babb7bf2760936148947ebfd5dbb07f1f8d8509
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2023-01-24 22:21:47 +0100

    Comment - utworzenie dto oraz mapera dla modelu. Dodanie adnotacji walidacyjnych dla atrybutów klasy oraz kontrolera.

--- Java/E-montazysta/src/main/java/com/emontazysta/model/Comment.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/model/Comment.java
@@ -5,15 +5,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
 import java.util.Date;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Comment {
@@ -22,7 +23,9 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String content;
 
+    @NotNull
     private Date createdAt;
 }
