6671bd9688bc42d4e68454bd6aa4ee139a6ce136
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2023-01-24 20:08:22 +0100

    Utworzenie dto oraz mapera dla modelu. Dodanie adnotacji walidacyjnych dla atrybutów klasy oraz kontrolera

--- Java/E-montazysta/src/main/java/com/emontazysta/model/Attachment.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/model/Attachment.java
@@ -1,18 +1,19 @@
 package com.emontazysta.model;
 
 import com.emontazysta.enums.TypeOfAttachment;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Attachment {
@@ -21,13 +22,18 @@ public class Attachment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String name;
 
+    @NotBlank
     private String url;
 
+    @NotBlank
     private String description;
 
+    @NotNull
     private TypeOfAttachment typeOfAttachment;
 
+    @NotNull
     private Date createdAt;
 }
