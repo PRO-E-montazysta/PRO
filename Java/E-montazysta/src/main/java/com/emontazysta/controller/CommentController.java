1db793a36427f672e661a7c8d4f986e7b1e609c1
Tomasz Pyrzak <75432078+s21670-Pyrzak-Tomasz-gr-18C@users.noreply.github.com>
2023-01-25 12:51:09 +0100

    Comment - utworzenie dto oraz mapera dla modelu. Dodanie adnotacji wa… (#24)
    * Comment - utworzenie dto oraz mapera dla modelu. Dodanie adnotacji walidacyjnych dla atrybutów klasy oraz kontrolera.
    
    * Removed bad annotation; added generating createdAt; injecting interface
    
    Co-authored-by: Tomasz <75432078+tpyrzak@users.noreply.github.com>
    Co-authored-by: Kamil Kuryłek <kurylek98@gmail.com>

--- Java/E-montazysta/src/main/java/com/emontazysta/controller/CommentController.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/controller/CommentController.java
@@ -1,15 +1,20 @@
 package com.emontazysta.controller;
 
+import com.emontazysta.mapper.CommentMapper;
 import com.emontazysta.model.Comment;
-import com.emontazysta.service.impl.CommentServiceImpl;
+import com.emontazysta.model.dto.CommentDto;
+import com.emontazysta.service.CommentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.emontazysta.configuration.Constants.API_BASE_CONSTANT;
 
@@ -18,15 +23,17 @@ import static com.emontazysta.configuration.Constants.API_BASE_CONSTANT;
 @RequestMapping(value = API_BASE_CONSTANT + "/comments", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CommentController {
 
-    private final CommentServiceImpl commentService;
+    private final CommentService commentService;
 
-    @GetMapping
+    @GetMapping("/all")
     @Operation(description = "Allows to get all Comments.", security = @SecurityRequirement(name = "bearer-key"))
-    public List<Comment> getAll() {
-        return commentService.getAll();
+    public ResponseEntity<List<CommentDto>> getAll() {
+        return ResponseEntity.ok().body(commentService.getAll().stream()
+                .map(CommentMapper::commentToDto)
+                .collect(Collectors.toList()));
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     @Operation(description = "Allows to get Comment by given Id.", security = @SecurityRequirement(name = "bearer-key"))
     public Comment getById(@PathVariable("id") Long id) {
         return commentService.getById(id);
@@ -35,11 +42,11 @@ public class CommentController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(description = "Allows to add new Comment.", security = @SecurityRequirement(name = "bearer-key"))
-    public void add(@RequestBody Comment comment) {
+    public void add(@Valid @RequestBody Comment comment) {
         commentService.add(comment);
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}")
     @Operation(description = "Allows to delete Comment by given Id.", security = @SecurityRequirement(name = "bearer-key"))
     public void deleteById(@PathVariable("id") Long id) {
         commentService.delete(id);
