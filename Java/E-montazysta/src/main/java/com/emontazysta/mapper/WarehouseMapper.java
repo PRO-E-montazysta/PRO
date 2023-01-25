d5200b2d56ba97c3247c5e0e6998615b1c31bbba
Kamil Kuryłek <46301983+kurylek@users.noreply.github.com>
2023-01-25 23:55:18 +0100

    Add DTO, Mapper and Validation (#25)

---
+++ Java/E-montazysta/src/main/java/com/emontazysta/mapper/WarehouseMapper.java
@@ -0,0 +1,16 @@
+package com.emontazysta.mapper;
+
+import com.emontazysta.model.Warehouse;
+import com.emontazysta.model.dto.WarehouseDto;
+
+public class WarehouseMapper {
+
+    public static WarehouseDto warehouseToDto(Warehouse warehouse) {
+        return WarehouseDto.builder()
+                .id(warehouse.getId())
+                .name(warehouse.getName())
+                .description(warehouse.getDescription())
+                .openingHours(warehouse.getOpeningHours())
+                .build();
+    }
+}
