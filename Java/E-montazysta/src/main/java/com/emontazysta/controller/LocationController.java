ba0bad3ada7b7bbb1add99e7172375f8121f9a4b
Kamil Kuryłek <kurylek98@gmail.com>
2023-01-27 23:03:11 +0100

    Add Validation & use DTO in Controller

--- Java/E-montazysta/src/main/java/com/emontazysta/controller/LocationController.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/controller/LocationController.java
@@ -1,6 +1,8 @@
 package com.emontazysta.controller;
 
+import com.emontazysta.mapper.LocationMapper;
 import com.emontazysta.model.Location;
+import com.emontazysta.model.dto.LocationDto;
 import com.emontazysta.service.impl.LocationServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -9,7 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.emontazysta.configuration.Constants.API_BASE_CONSTANT;
 
@@ -22,20 +26,22 @@ public class LocationController {
 
     @GetMapping("/all")
     @Operation(description = "Allows to get all Locations.", security = @SecurityRequirement(name = "bearer-key"))
-    public List<Location> getAll() {
-        return locationService.getAll();
+    public List<LocationDto> getAll() {
+        return locationService.getAll().stream()
+                .map(LocationMapper::locationToDto)
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
     @Operation(description = "Allows to get Location by given Id.", security = @SecurityRequirement(name = "bearer-key"))
-    public Location getById(@PathVariable("id") Long id) {
-        return locationService.getById(id);
+    public LocationDto getById(@PathVariable("id") Long id) {
+        return LocationMapper.locationToDto(locationService.getById(id));
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(description = "Allows to add new Location.", security = @SecurityRequirement(name = "bearer-key"))
-    public void add(@RequestBody Location location) {
+    public void add(@Valid @RequestBody Location location) {
         locationService.add(location);
     }
 
@@ -48,7 +54,7 @@ public class LocationController {
     @PutMapping("/{id}")
     @Operation(description = "Allows to update Location by given Id, and Location.", security = @SecurityRequirement(name = "bearer-key"))
     public void update(@PathVariable("id") Long id,
-                               @RequestBody Location location) {
+                               @Valid @RequestBody Location location) {
         locationService.update(id, location);
     }
 }
--- Java/E-montazysta/src/main/java/com/emontazysta/model/Location.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/model/Location.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Data
@@ -19,6 +20,7 @@ public class Location {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String name;
 
     private Double xCoordinate;
