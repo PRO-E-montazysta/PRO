80dce344554c519c4bffcfdcba3910c564bc3b54
Kamil Kuryłek <46301983+kurylek@users.noreply.github.com>
2023-01-28 11:35:42 +0100

    CU-862j4wv45_Location-class-validation (#39)
    * Create DTO & Mapper
    
    * Fix Mappings
    
    * Add Validation & use DTO in Controller

--- Java/E-montazysta/src/main/java/com/emontazysta/controller/LocationController.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/controller/LocationController.java
@@ -1,6 +1,8 @@
 package com.emontazysta.controller;
 
+import com.emontazysta.mapper.LocationMapper;
 import com.emontazysta.model.Location;
+import com.emontazysta.model.dto.LocationDto;
 import com.emontazysta.service.impl.LocationServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -9,7 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.emontazysta.configuration.Constants.API_BASE_CONSTANT;
 
@@ -20,35 +24,37 @@ public class LocationController {
 
     private final LocationServiceImpl locationService;
 
-    @GetMapping
+    @GetMapping("/all")
     @Operation(description = "Allows to get all Locations.", security = @SecurityRequirement(name = "bearer-key"))
-    public List<Location> getAll() {
-        return locationService.getAll();
+    public List<LocationDto> getAll() {
+        return locationService.getAll().stream()
+                .map(LocationMapper::locationToDto)
+                .collect(Collectors.toList());
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     @Operation(description = "Allows to get Location by given Id.", security = @SecurityRequirement(name = "bearer-key"))
-    public Location getById(@PathVariable("id") Long id) {
-        return locationService.getById(id);
+    public LocationDto getById(@PathVariable("id") Long id) {
+        return LocationMapper.locationToDto(locationService.getById(id));
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(description = "Allows to add new Location.", security = @SecurityRequirement(name = "bearer-key"))
-    public void add(@RequestBody Location location) {
+    public void add(@Valid @RequestBody Location location) {
         locationService.add(location);
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}")
     @Operation(description = "Allows to delete Location by given Id.", security = @SecurityRequirement(name = "bearer-key"))
     public void deleteById(@PathVariable("id") Long id) {
         locationService.delete(id);
     }
 
-    @PutMapping("{id}")
+    @PutMapping("/{id}")
     @Operation(description = "Allows to update Location by given Id, and Location.", security = @SecurityRequirement(name = "bearer-key"))
     public void update(@PathVariable("id") Long id,
-                               @RequestBody Location location) {
+                               @Valid @RequestBody Location location) {
         locationService.update(id, location);
     }
 }
