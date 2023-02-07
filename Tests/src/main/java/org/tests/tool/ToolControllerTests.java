86bd4a25812c57d29a0113bc0bf5263616ac8863
WIN <karolsobotka@protonmail.com>
2023-02-07 22:13:32 +0100

    delete test, ordered tests

--- Tests/src/main/java/org/tests/tool/ToolControllerTests.java
+++ Tests/src/main/java/org/tests/tool/ToolControllerTests.java
@@ -1,62 +1,94 @@
 package org.tests.tool;
 
-import com.emontazysta.configuration.SecurityProperties;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.tests.SetUp;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 
 public class ToolControllerTests {
+    private static String names[] = new String[]{"screwdriver", "drill", "knife", "lopata", "dluto"};
+    private static int[] indexRangeForDeletion;
 
-    String requestBody ="{\n" +
-            "  \"name\": \"costam\"\n" +
-            "}";
+    private String bearerToken = SetUp.getBearerToken();
+
 
     @Test
+    @Order(1)
+    public void postTool(){
+
+        for (String s : names) {
+            String requestBody ="" +
+                    "{\n" +
+                    "  \"name\": \""+s+"\"\n" +
+                    "}";
+            Response response =
+                    given()
+                            .headers(
+                                    "Authorization","Bearer " + bearerToken,
+                                    "Content-Type", ContentType.JSON,
+                                    "Accept", ContentType.JSON
+                            )
+                            .and()
+                            .body(requestBody)
+                            .when()
+                            .post("/api/v1/tools");
+
+            Assertions.assertEquals(HttpStatus.SC_CREATED, response.statusCode());
+            System.out.println("wstawiono "+ s);
+        }
+
+
+    }
+    @Test
+    @Order(2)
     public void checkIfToolIsInDB() {
+        int i = 0;
+        for (String toolName : names) {
+            given()
+                    .headers(
+                            "Authorization","Bearer " + bearerToken,
+                            "Content-Type",ContentType.JSON,
+                            "Accept", ContentType.JSON)
 
+                    .get("/api/v1/tools/all").
+                    prettyPeek().
+                    then().
+                    statusCode(HttpStatus.SC_OK).
+                    rootPath("["+i+"]")
+                    .body("name", equalTo(toolName))
+                    .body("code", matchesPattern("^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$"));
 
-        given()
-                .headers(
-                        "Authorization","Bearer " + SetUp.getBearerToken(),
-                        "Content-Type",ContentType.JSON,
-                        "Accept", ContentType.JSON)
-
-        .get("/api/v1/tools/all").
-            prettyPeek().
-            then().
-               statusCode(HttpStatus.SC_OK).
-                    rootPath("[0]")
-                        .body("name", equalTo("costam"));
-//                        .body("code", equalTo("PL"));
-
+                    i++;
+        }
 
     }
 
     @Test
-    public void postTool(){
-       Response response =  given()
-                .headers(
-                        "Authorization","Bearer " + SetUp.getBearerToken(),
-                        "Content-Type", ContentType.JSON,
-                        "Accept", ContentType.JSON
-                        )
-            .and()
-                .body(requestBody)
-                .when()
-                .post("/api/v1/tools")
-                .then()
-                .extract().response();
+    @Order(3)
+    public void deleteToolTest(){
 
+        for (int i = indexRangeForDeletion[0]; i < indexRangeForDeletion[1]; i++) {
 
+            Response response = given()
+                    .headers(
+                            "Authorization","Bearer " + bearerToken,
+                            "Content-Type", ContentType.JSON,
+                            "Accept", ContentType.JSON
+                    )
+                    .and()
+                    .delete("/api/v1/tools/"+i)
+                    .then()
+                    .extract().response();
 
-//        Assertions.assertEquals(201, response.statusCode());
-//        Assertions.assertEquals("śrubokręt", response.jsonPath().getString("name"));
+            response.prettyPrint();
+            System.out.println("usunieto narzedzie o indeksie: "+i);
+            Assertions.assertEquals(HttpStatus.SC_OK, response.statusCode());
+        }
 
     }
 }
