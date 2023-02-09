cd3f539fbe749a728eac4210c05396811032ed8b
Kryhowsky <piter71-1997@o2.pl>
2023-02-09 20:33:49 +0100

    Added constructors where Lombok is unwell

--- Java/E-montazysta/src/main/java/com/emontazysta/model/WarehouseManager.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/model/WarehouseManager.java
@@ -1,5 +1,6 @@
 package com.emontazysta.model;
 
+import com.emontazysta.enums.Role;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @Entity
@@ -16,6 +18,20 @@ import java.util.List;
 @AllArgsConstructor
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class WarehouseManager extends Warehouseman {
+
+    public WarehouseManager(Long id, String firstName, String lastName, String email, String password, String username,
+                            String resetPasswordToken, Set<Role> roles, String phone, String pesel,
+                            List<Unavailability> unavailabilities, List<Notification> notifications,
+                            List<Comment> employeeComments, List<ElementEvent> elementEvents, List<Employment> employments,
+                            List<Attachment> attachments, List<ToolEvent> toolEvents, List<ToolRelease> releasedTools,
+                            List<ElementReturnRelease> elementReturnReleases, List<DemandAdHoc> demandAdHocs,
+                            List<DemandAdHoc> mngDemandAdHocs) {
+
+        super(id, firstName, lastName, email, password, username, resetPasswordToken, roles, phone, pesel,
+                unavailabilities, notifications, employeeComments, elementEvents, employments, attachments, toolEvents,
+                releasedTools, elementReturnReleases, demandAdHocs);
+        this.demandAdHocs = mngDemandAdHocs;
+    }
 
     @OneToMany(mappedBy = "warehouseManager")
     private List<DemandAdHoc> demandAdHocs;
