860b80b3d9839f33198a309a4c2273299133eae4
Kryhowsky <40215487+Kryhowsky@users.noreply.github.com>
2023-02-09 20:56:45 +0100

    Added constructors where Lombok is unwell (#61)

--- Java/E-montazysta/src/main/java/com/emontazysta/model/Foreman.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/model/Foreman.java
@@ -1,6 +1,7 @@
 package com.emontazysta.model;
 
 
+import com.emontazysta.enums.Role;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @Entity
@@ -17,6 +19,23 @@ import java.util.List;
 @AllArgsConstructor
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Foreman extends Fitter {
+
+    public Foreman(Long id, String firstName, String lastName, String email, String password, String username,
+                  String resetPasswordToken, Set<Role> roles, String phone, String pesel,
+                  List<Unavailability> unavailabilities, List<Notification> notifications,
+                  List<Comment> employeeComments, List<ElementEvent> elementEvents, List<Employment> employments,
+                  List<Attachment> attachments, List<ToolEvent> toolEvents, List<OrderStage> workingOn,
+                  List<OrderStage> ordersStagesList, List<ToolRelease> receivedTools, List<Orders> assignedOrders,
+                  List<ElementReturnRelease> elementReturnReleases, List<DemandAdHoc> demandsAdHocs) {
+
+        super(id, firstName, lastName, email, password, username, resetPasswordToken, roles, phone, pesel,
+                unavailabilities, notifications, employeeComments, elementEvents, employments, attachments, toolEvents, workingOn);
+        this.ordersStagesList = ordersStagesList;
+        this.receivedTools = receivedTools;
+        this.assignedOrders = assignedOrders;
+        this.elementReturnReleases = elementReturnReleases;
+        this.demandsAdHocs = demandsAdHocs;
+    }
 
     @OneToMany(mappedBy = "managedBy")
     private List<OrderStage> ordersStagesList;
