af100d298fa7bc63219bc9e820a8e8baa0c143ff
Kryhowsky <piter71-1997@o2.pl>
2023-02-11 11:25:48 +0100

    Moved attachmentmapper usage controller > service

--- Java/E-montazysta/src/main/java/com/emontazysta/controller/AttachmentController.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/controller/AttachmentController.java
@@ -1,6 +1,5 @@
 package com.emontazysta.controller;
 
-import com.emontazysta.mapper.AttachmentMapper;
 import com.emontazysta.model.dto.AttachmentDto;
 import com.emontazysta.service.AttachmentService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.emontazysta.configuration.Constants.API_BASE_CONSTANT;
 
@@ -23,27 +21,24 @@ import static com.emontazysta.configuration.Constants.API_BASE_CONSTANT;
 public class AttachmentController {
 
     private final AttachmentService attachmentService;
-    private final AttachmentMapper attachmentMapper;
 
     @GetMapping("/all")
     @Operation(description = "Allows to get all Attachments.", security = @SecurityRequirement(name = "bearer-key"))
     public ResponseEntity<List<AttachmentDto>> getAll() {
-        return ResponseEntity.ok().body(attachmentService.getAll().stream()
-                .map(attachmentMapper::attachmentDto)
-                .collect(Collectors.toList()));
+        return ResponseEntity.ok().body(attachmentService.getAll());
     }
 
     @GetMapping("/{id}")
     @Operation(description = "Allows to get Attachment by given Id.", security = @SecurityRequirement(name = "bearer-key"))
     public ResponseEntity<AttachmentDto> getById(@PathVariable Long id) {
-        return ResponseEntity.ok().body(attachmentMapper.attachmentDto(attachmentService.getById(id)));
+        return ResponseEntity.ok().body(attachmentService.getById(id));
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(description = "Allows to add new Attachment.", security = @SecurityRequirement(name = "bearer-key"))
     public AttachmentDto add(@Valid @RequestBody AttachmentDto attachmentDto) {
-        return attachmentMapper.attachmentDto(attachmentService.add(attachmentMapper.toEntity(attachmentDto)));
+        return attachmentService.add(attachmentDto);
     }
 
     @DeleteMapping("/{id}")
