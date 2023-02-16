854744957fd03678a9ca1b1bffc160e9e3546202
Kryhowsky <piter71-1997@o2.pl>
2023-02-16 19:42:21 +0100

    SpecialistDTO

--- Java/E-montazysta/src/main/java/com/emontazysta/mapper/SpecialistMapper.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/mapper/SpecialistMapper.java
@@ -1,11 +1,26 @@
 package com.emontazysta.mapper;
 
+import com.emontazysta.model.DemandAdHoc;
+import com.emontazysta.model.Orders;
 import com.emontazysta.model.Specialist;
 import com.emontazysta.model.dto.SpecialistDto;
+import com.emontazysta.repository.DemandAdHocRepository;
+import com.emontazysta.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
 public class SpecialistMapper {
 
-    public static SpecialistDto toDto(Specialist specialist) {
+    private final OrderRepository orderRepository;
+    private final DemandAdHocRepository demandAdHocRepository;
+
+    public SpecialistDto toDto(Specialist specialist) {
 
         return SpecialistDto.builder()
                 .id(specialist.getId())
@@ -15,6 +30,30 @@ public class SpecialistMapper {
                 .email(specialist.getEmail())
                 .phone(specialist.getPhone())
                 .pesel(specialist.getPesel())
+                .orders(specialist.getOrders().stream().map(Orders::getId).collect(Collectors.toList()))
+                .demandAdHocs(specialist.getDemandAdHocs().stream().map(DemandAdHoc::getId).collect(Collectors.toList()))
                 .build();
+    }
+
+    public Specialist toEntity(SpecialistDto specialistDto) {
+
+        List<Orders> ordersList = new ArrayList<>();
+        specialistDto.getOrders().forEach(orderId -> ordersList.add(orderRepository.getReferenceById(orderId)));
+
+        List<DemandAdHoc> demandAdHocList = new ArrayList<>();
+        specialistDto.getDemandAdHocs().forEach(demandAdHocId -> demandAdHocList.add(demandAdHocRepository.getReferenceById(demandAdHocId)));
+
+        Specialist specialist = new Specialist();
+        specialist.setId(specialistDto.getId());
+        specialist.setFirstName(specialistDto.getFirstName());
+        specialist.setLastName(specialistDto.getLastName());
+        specialist.setUsername(specialistDto.getUsername());
+        specialist.setEmail(specialistDto.getEmail());
+        specialist.setPhone(specialistDto.getPhone());
+        specialist.setPesel(specialistDto.getPesel());
+        specialist.setOrders(ordersList);
+        specialist.setDemandAdHocs(demandAdHocList);
+
+        return specialist;
     }
 }
--- Java/E-montazysta/src/main/java/com/emontazysta/service/impl/SpecialistServiceImpl.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/service/impl/SpecialistServiceImpl.java
@@ -1,6 +1,8 @@
 package com.emontazysta.service.impl;
 
+import com.emontazysta.mapper.SpecialistMapper;
 import com.emontazysta.model.Specialist;
+import com.emontazysta.model.dto.SpecialistDto;
 import com.emontazysta.repository.SpecialistRepository;
 import com.emontazysta.service.SpecialistService;
 import lombok.RequiredArgsConstructor;
@@ -8,28 +10,32 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class SpecialistServiceImpl implements SpecialistService {
 
     private final SpecialistRepository repository;
-
+    private final SpecialistMapper specialistMapper;
 
     @Override
-    public List<Specialist> getAll() {
-        return repository.findAll();
+    public List<SpecialistDto> getAll() {
+        return repository.findAll().stream()
+                .map(specialistMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Specialist getById(Long id) {
-        return repository.findById(id)
-                         .orElseThrow(EntityNotFoundException::new);
+    public SpecialistDto getById(Long id) {
+        Specialist specialist = repository.findById(id).orElseThrow(EntityNotFoundException::new);
+        return specialistMapper.toDto(specialist);
     }
 
     @Override
-    public void add(Specialist specialist) {
-        repository.save(specialist);
+    public SpecialistDto add(SpecialistDto specialistDto) {
+        Specialist specialist = specialistMapper.toEntity(specialistDto);
+        return specialistMapper.toDto(repository.save(specialist));
     }
 
     @Override
