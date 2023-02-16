06b13fb6382d1f8c05c1d4f50313543d738d6a5c
Kryhowsky <piter71-1997@o2.pl>
2023-02-16 21:21:58 +0100

    Smal cleanups

--- Java/E-montazysta/src/main/java/com/emontazysta/mapper/SpecialistMapper.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/mapper/SpecialistMapper.java
@@ -1,11 +1,25 @@
 package com.emontazysta.mapper;
 
+import com.emontazysta.model.Attachment;
+import com.emontazysta.model.Comment;
 import com.emontazysta.model.DemandAdHoc;
+import com.emontazysta.model.ElementEvent;
+import com.emontazysta.model.Employment;
+import com.emontazysta.model.Notification;
 import com.emontazysta.model.Orders;
 import com.emontazysta.model.Specialist;
+import com.emontazysta.model.ToolEvent;
+import com.emontazysta.model.Unavailability;
 import com.emontazysta.model.dto.SpecialistDto;
+import com.emontazysta.repository.AttachmentRepository;
+import com.emontazysta.repository.CommentRepository;
 import com.emontazysta.repository.DemandAdHocRepository;
+import com.emontazysta.repository.ElementEventRepository;
+import com.emontazysta.repository.EmploymentRepository;
+import com.emontazysta.repository.NotificationRepository;
 import com.emontazysta.repository.OrderRepository;
+import com.emontazysta.repository.ToolEventRepository;
+import com.emontazysta.repository.UnavailabilityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +31,13 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class SpecialistMapper {
 
+    private final UnavailabilityRepository unavailabilityRepository;
+    private final NotificationRepository notificationRepository;
+    private final CommentRepository commentRepository;
+    private final ElementEventRepository elementEventRepository;
+    private final EmploymentRepository employmentRepository;
+    private final AttachmentRepository attachmentRepository;
+    private final ToolEventRepository toolEventRepository;
     private final OrderRepository orderRepository;
     private final DemandAdHocRepository demandAdHocRepository;
 
@@ -30,12 +51,40 @@ public class SpecialistMapper {
                 .email(specialist.getEmail())
                 .phone(specialist.getPhone())
                 .pesel(specialist.getPesel())
+                .unavailabilities(specialist.getUnavailabilities().stream().map(Unavailability::getId).collect(Collectors.toList()))
+                .notifications(specialist.getNotifications().stream().map(Notification::getId).collect(Collectors.toList()))
+                .employeeComments(specialist.getEmployeeComments().stream().map(Comment::getId).collect(Collectors.toList()))
+                .elementEvents(specialist.getElementEvents().stream().map(ElementEvent::getId).collect(Collectors.toList()))
+                .employments(specialist.getEmployments().stream().map(Employment::getId).collect(Collectors.toList()))
+                .attachments(specialist.getAttachments().stream().map(Attachment::getId).collect(Collectors.toList()))
+                .toolEvents(specialist.getToolEvents().stream().map(ToolEvent::getId).collect(Collectors.toList()))
                 .orders(specialist.getOrders().stream().map(Orders::getId).collect(Collectors.toList()))
                 .demandAdHocs(specialist.getDemandAdHocs().stream().map(DemandAdHoc::getId).collect(Collectors.toList()))
                 .build();
     }
 
     public Specialist toEntity(SpecialistDto specialistDto) {
+
+        List<Unavailability> unavailabilityList = new ArrayList<>();
+        specialistDto.getUnavailabilities().forEach(unavailabilityId -> unavailabilityList.add(unavailabilityRepository.getReferenceById(unavailabilityId)));
+
+        List<Notification> notificationList = new ArrayList<>();
+        specialistDto.getNotifications().forEach(notificationId -> notificationList.add(notificationRepository.getReferenceById(notificationId)));
+
+        List<Comment> employeeCommentsList = new ArrayList<>();
+        specialistDto.getEmployeeComments().forEach(commentId -> employeeCommentsList.add(commentRepository.getReferenceById(commentId)));
+
+        List<ElementEvent> elementEventList = new ArrayList<>();
+        specialistDto.getElementEvents().forEach(elementEventId -> elementEventList.add(elementEventRepository.getReferenceById(elementEventId)));
+
+        List<Employment> employmentList = new ArrayList<>();
+        specialistDto.getEmployments().forEach(employmentId -> employmentList.add(employmentRepository.getReferenceById(employmentId)));
+
+        List<Attachment> attachmentList = new ArrayList<>();
+        specialistDto.getAttachments().forEach(attachmentId -> attachmentList.add(attachmentRepository.getReferenceById(attachmentId)));
+
+        List<ToolEvent> toolEventList = new ArrayList<>();
+        specialistDto.getToolEvents().forEach(toolEventId -> toolEventList.add(toolEventRepository.getReferenceById(toolEventId)));
 
         List<Orders> ordersList = new ArrayList<>();
         specialistDto.getOrders().forEach(orderId -> ordersList.add(orderRepository.getReferenceById(orderId)));
@@ -51,6 +100,13 @@ public class SpecialistMapper {
         specialist.setEmail(specialistDto.getEmail());
         specialist.setPhone(specialistDto.getPhone());
         specialist.setPesel(specialistDto.getPesel());
+        specialist.setUnavailabilities(unavailabilityList);
+        specialist.setNotifications(notificationList);
+        specialist.setEmployeeComments(employeeCommentsList);
+        specialist.setElementEvents(elementEventList);
+        specialist.setEmployments(employmentList);
+        specialist.setAttachments(attachmentList);
+        specialist.setToolEvents(toolEventList);
         specialist.setOrders(ordersList);
         specialist.setDemandAdHocs(demandAdHocList);
 
