7f40a64d8cf49f7f3d456258280859cbe41a988f
Kryhowsky <piter71-1997@o2.pl>
2023-02-16 20:11:13 +0100

    ToolEventDTO

--- Java/E-montazysta/src/main/java/com/emontazysta/service/impl/ToolEventServiceImpl.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/service/impl/ToolEventServiceImpl.java
@@ -1,30 +1,38 @@
 package com.emontazysta.service.impl;
 
+import com.emontazysta.mapper.ToolEventMapper;
 import com.emontazysta.model.ToolEvent;
+import com.emontazysta.model.dto.ToolEventDto;
 import com.emontazysta.repository.ToolEventRepository;
 import com.emontazysta.service.ToolEventService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class ToolEventServiceImpl implements ToolEventService {
 
     private final ToolEventRepository repository;
+    private final ToolEventMapper toolEventMapper;
 
-    public List<ToolEvent> getAll() {
-        return repository.findAll();
+    public List<ToolEventDto> getAll() {
+        return repository.findAll().stream()
+                .map(toolEventMapper::toDto)
+                .collect(Collectors.toList());
     }
 
-    public ToolEvent getById(Long id) {
-        return repository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Tool event with id " + id + " not found!"));
+    public ToolEventDto getById(Long id) {
+        ToolEvent toolEvent = repository.findById(id).orElseThrow(() -> new RuntimeException("Tool event with id " + id + " not found!"));
+        return toolEventMapper.toDto(toolEvent);
 
     }
 
-    public void add(ToolEvent event) {
-        repository.save(event);
+    public ToolEventDto add(ToolEventDto toolEventDto) {
+        ToolEvent toolEvent = toolEventMapper.toEntity(toolEventDto);
+        return toolEventMapper.toDto(repository.save(toolEvent));
     }
 
     public void delete(Long id) {
