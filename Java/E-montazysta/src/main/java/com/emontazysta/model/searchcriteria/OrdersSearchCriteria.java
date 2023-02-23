75cf14511e6bb4bb977c5e0a087a2d90a88c7afa
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2023-02-23 20:50:02 +0100

    filtrowanie paginacja i sortowanie dla kalsy Orders OrdersSearchCriteria

--- Java/E-montazysta/src/main/java/com/emontazysta/model/searchcriteria/OrdersSearchCriteria.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/model/searchcriteria/OrdersSearchCriteria.java
@@ -1,4 +1,17 @@
 package com.emontazysta.model.searchcriteria;
 
+import com.emontazysta.enums.TypeOfPriority;
+import com.emontazysta.enums.TypeOfStatus;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
 public class OrdersSearchCriteria {
+
+    private String name;
+    private List<TypeOfStatus> typeOfStatus;
+    private List<TypeOfPriority> typeOfPriority;
+    private Date plannedStart;
 }
