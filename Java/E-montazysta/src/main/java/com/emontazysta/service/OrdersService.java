d614d45302f8bf7510cf5f2315db8e5f47edfef7
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2023-02-23 20:44:32 +0100

    filtrowanie paginacja i sortowanie dla kalsy Orders OrdersService

--- Java/E-montazysta/src/main/java/com/emontazysta/service/OrdersService.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/service/OrdersService.java
@@ -1,6 +1,7 @@
 package com.emontazysta.service;
 
 import com.emontazysta.model.dto.OrdersDto;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -11,4 +12,6 @@ public interface OrdersService {
     OrdersDto add(OrdersDto orders);
     void delete(Long id);
     OrdersDto update(Long id, OrdersDto orders);
+
+    public Page<OrdersDto> getFilteredOrders(OrdersPage ordersPage, OrdersSearchCriteria ordersSearchCriteria);
 }
