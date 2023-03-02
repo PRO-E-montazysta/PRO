600bec43d1d7e06aecc54c18769fda09e4d1de2b
Kamil Kuryłek <kurylek98@gmail.com>
2023-03-02 10:49:17 +0100

    Update endpoint

--- React/src/api/tool.api.ts
+++ React/src/api/tool.api.ts
@@ -8,6 +8,6 @@ export const getToolDetails = (id?: string) => {
     return makeServiceCall(`/tools/${id}`, 'GET', {});
 }
 
-export const getToolsFromWarehouse = (warehouseId?: string) => {
-    return makeServiceCall(`/tools/all/?warehouseId=${warehouseId}`, 'GET', {});
+export const getToolsFromWarehouse = (warehouseId: string, payload: PayloadProps) => {
+    return makeServiceCall(`/tools/tools-from-warehouse/${warehouseId}`, 'GET', { ...payload });
 }
