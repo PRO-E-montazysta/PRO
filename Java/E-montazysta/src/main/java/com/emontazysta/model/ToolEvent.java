d55baedc9a4791a6f20a4d73cd69ea6b54321588
Kamil Kuryłek <46301983+kurylek@users.noreply.github.com>
2023-03-05 13:30:20 +0100

    Change Id genration type (#89)

--- Java/E-montazysta/src/main/java/com/emontazysta/model/ToolEvent.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/model/ToolEvent.java
@@ -23,7 +23,7 @@ public class ToolEvent {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private LocalDateTime eventDate;
