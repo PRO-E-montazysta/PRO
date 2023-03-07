7295a8ceda6fe2bcfd031267d499bb42553422f8
Marcin Hojarski <hojarski.m@gmail.com>
2023-03-07 21:51:43 +0100

    clean emp details

--- React/src/pages/employees/employeesDetails/EmpDetails.tsx
+++ React/src/pages/employees/employeesDetails/EmpDetails.tsx
@@ -1,29 +1,18 @@
 import { useQuery } from 'react-query'
 import { AxiosError } from 'axios'
-import { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
 import { Employee } from '../../../types/model/Employee'
 import { getUser } from '../../../api/user.api'
-// import { Avatar, Card, CardContent, CardHeader, CardMedia, IconButton, Typography } from '@mui/material';
-// import FavoriteIcon from '@mui/icons-material/Favorite';
-// import ShareIcon from '@mui/icons-material/Share';
-// import MoreVertIcon from '@mui/icons-material/MoreVert';
-// import { styled } from '@mui/material/styles';
-
-import * as React from 'react'
 import { styled } from '@mui/material/styles'
 import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
-import CardMedia from '@mui/material/CardMedia'
 import CardContent from '@mui/material/CardContent'
 import CardActions from '@mui/material/CardActions'
 import Collapse from '@mui/material/Collapse'
 import Avatar from '@mui/material/Avatar'
 import IconButton, { IconButtonProps } from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
-import { red } from '@mui/material/colors'
-import FavoriteIcon from '@mui/icons-material/Favorite'
-import ShareIcon from '@mui/icons-material/Share'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 import WorkHistoryIcon from '@mui/icons-material/WorkHistory'
