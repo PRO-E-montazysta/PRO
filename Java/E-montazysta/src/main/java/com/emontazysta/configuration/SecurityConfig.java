36173a7ae0f6965cce30c8b224c88b4a2ff319e1
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2023-03-09 22:16:44 +0100

    Poprawka filtrowania dla klasy Orders, dodanie obsługi błedów zwiazanych z błednymi danymi rozpoczęcia i zakończenia oraz wyszukiwania przez pracownika nie przypisanego do żadnej firmy, dodanie filtrowania tylko w zakresie firmy zalogowanego użytkownika z uuwzględnieniem CLOUD_ADMINA, dodatkowe dane w seedowaniu, dodanie wszustkich ról dla uzytkownika admin

--- Java/E-montazysta/src/main/java/com/emontazysta/configuration/SecurityConfig.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/configuration/SecurityConfig.java
@@ -63,7 +63,8 @@ public class SecurityConfig {
                         securityProperties.getCloudManagerPassword(),
                         securityProperties.getCloudManagerUsername(),
                         null,
-                        Set.of(Role.CLOUD_ADMIN)));
+                        Set.of(Role.CLOUD_ADMIN,Role.ADMIN,Role.SALES_REPRESENTATIVE,Role.FITTER, Role.FOREMAN,
+                                Role.MANAGER, Role.SPECIALIST, Role.WAREHOUSE_MANAGER, Role.WAREHOUSE_MAN)));
     }
 
     @Bean
--- Java/E-montazysta/src/main/java/com/emontazysta/service/OrdersService.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/service/OrdersService.java
@@ -3,7 +3,7 @@ package com.emontazysta.service;
 import com.emontazysta.model.dto.OrdersCompanyManagerDto;
 import com.emontazysta.model.dto.OrdersDto;
 import com.emontazysta.model.searchcriteria.OrdersSearchCriteria;
-
+import java.security.Principal;
 import java.util.List;
 
 public interface OrdersService {
@@ -14,5 +14,5 @@ public interface OrdersService {
     void delete(Long id);
     OrdersDto update(Long id, OrdersDto orders);
 
-    public List<OrdersCompanyManagerDto> getFilteredOrders(OrdersSearchCriteria ordersSearchCriteria);
+    public List<OrdersCompanyManagerDto> getFilteredOrders(OrdersSearchCriteria ordersSearchCriteria, Principal principal);
 }
