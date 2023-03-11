2b0a0050095dfb65ef9e52e34a9a094412eff768
Kamil Kuryłek <kurylek98@gmail.com>
2023-03-11 12:10:16 +0100

    Prettier

--- React/src/api/toolType.api.ts
+++ React/src/api/toolType.api.ts
@@ -1,13 +1,13 @@
-import { makeServiceCall, PayloadProps } from "./utils.api";
+import { makeServiceCall, PayloadProps } from './utils.api'
 
 export const getAllToolTypes = () => {
-    return makeServiceCall('/tooltype/filter', 'GET', {});
+    return makeServiceCall('/tooltype/filter', 'GET', {})
 }
 
 export const getFilteredToolTypes = (payload: PayloadProps) => {
-    return makeServiceCall('/tooltype/filter', 'GET', { ...payload });
+    return makeServiceCall('/tooltype/filter', 'GET', { ...payload })
 }
 
 export const getToolTypeDetails = (id?: string) => {
-    return makeServiceCall(`/tooltype/${id}`, 'GET', {});
+    return makeServiceCall(`/tooltype/${id}`, 'GET', {})
 }
