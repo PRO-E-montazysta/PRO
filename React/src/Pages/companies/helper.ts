99d3a98f6f05827d0b197e3f0be8233517e86dc6
s18288 <81017505+s18288@users.noreply.github.com>
2023-03-11 12:53:39 +0100

    Cu 862j1hec4 cloud admin component s18288pejotedupl (#103)
    * Fixed paths
    
    * +routes
    
    * +components
    
    * +names changes
    
    * +Roles
    
    * !pages
    
    * CloudAdmin mock
    
    * +api; +CloudAdmin mock variables;
    
    * rebase
    
    * !rebase
    
    * !rename
    
    * +logic;
    
    * !rebase;
    
    * Update pageList
    
    * There is no /companies/filter right now
    
    * Update helper
    
    * Add company status
    
    * Prettier
    
    ---------
    
    Co-authored-by: Kamil Kuryłek <kurylek98@gmail.com>

---
+++ React/src/Pages/companies/helper.ts
@@ -0,0 +1,75 @@
+import { FilterInputType } from '../../components/table/filter/TableFilter'
+import { Company } from '../../types/model/Company'
+import { HeadCell } from '../../components/table/sort/SortedTableHeader'
+import { formatDate } from '../../helpers/format.helper'
+import { companyStatusName, companyStatusOptions } from '../../helpers/enum.helper'
+
+import * as yup from 'yup'
+
+export const headCells: Array<HeadCell<Company>> = [
+    {
+        type: 'string',
+        id: 'companyName',
+        label: 'Nazwa firmy',
+        disablePadding: false,
+        numeric: false,
+    },
+    {
+        type: 'string',
+        id: 'createdAt',
+        label: 'Data utworzenia',
+        disablePadding: false,
+        numeric: false,
+        formatFn: (date: string) => (date ? formatDate(date) : ''),
+    },
+    {
+        type: 'string',
+        id: 'status',
+        label: 'Status',
+        disablePadding: false,
+        numeric: false,
+        formatFn: (status: string) => companyStatusName(status),
+    },
+    {
+        type: 'string',
+        id: 'statusReason',
+        label: 'Uzasadnienie statusu',
+        disablePadding: false,
+        numeric: false,
+    },
+]
+
+export const filterInitStructure: Array<FilterInputType> = [
+    {
+        id: 'companyName',
+        value: '',
+        label: 'Nazwa firmy',
+        inputType: 'text',
+        typeValue: 'string',
+    },
+    {
+        id: 'status',
+        value: '',
+        label: 'Status',
+        inputType: 'multiselect',
+        typeValue: 'Array',
+        options: companyStatusOptions(),
+    },
+]
+
+export const emptyForm = {
+    id: null,
+    companyName: '',
+    createdAt: '',
+    status: '',
+    statusReason: '',
+    warehouses: [],
+    orders: [],
+    clients: [],
+    employments: [],
+}
+
+export const validationSchema = yup.object({
+    companyName: yup.string().min(2, 'Nazwa musi zawierać co najmniej 2 znaki').required('Wprowadź nazwę'),
+    status: yup.string().required('Wybierz status'),
+})
