6f7a61c6fef36f15b043b11cf074c2c33a51d69e
Kamil Kuryłek <kurylek98@gmail.com>
2023-03-16 21:40:39 +0100

    Update model

--- React/src/types/model/Client.ts
+++ React/src/types/model/Client.ts
@@ -1,6 +1,7 @@
-//Tutaj powinniśmy wrzucać Typy danych, jakie dostajemy z Backendu. Tutaj np. dla pracowników
-
 export type Client = {
-   id: number
-   name: string
-};
+    id: number
+    name: string
+    contactDetails: string
+    companyId: number
+    orders: Array<number>
+}
