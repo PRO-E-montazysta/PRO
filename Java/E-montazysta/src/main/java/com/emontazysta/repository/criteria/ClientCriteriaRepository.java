073e4caf44bb26aa5737d5c9b26d23384db07285
Tomasz Pyrzak <75432078+s21670-Pyrzak-Tomasz-gr-18C@users.noreply.github.com>
2023-03-17 14:07:54 +0100

    CU-862jem0hq_Client-Filtering (#119)
    * Filtrowanie dla klasy Client
    
    * Filtrowanie dla klasy Client poprawka kryteriów wyszukiwania
    
    ---------
    
    Co-authored-by: Tomasz <75432078+tpyrzak@users.noreply.github.com>

---
+++ Java/E-montazysta/src/main/java/com/emontazysta/repository/criteria/ClientCriteriaRepository.java
@@ -0,0 +1,80 @@
+package com.emontazysta.repository.criteria;
+
+import com.emontazysta.enums.Role;
+import com.emontazysta.mapper.ClientMapper;
+import com.emontazysta.model.AppUser;
+import com.emontazysta.model.Client;
+import com.emontazysta.model.Employment;
+import com.emontazysta.model.dto.ClientDto;
+import com.emontazysta.model.searchcriteria.ClientSearchCriteria;
+import com.emontazysta.service.AppUserService;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Repository
+public class ClientCriteriaRepository {
+
+    private final EntityManager entityManager;
+    private final CriteriaBuilder criteriaBuilder;
+    private final ClientMapper clientMapper;
+    private final AppUserService userService;
+
+    public ClientCriteriaRepository(EntityManager entityManager, ClientMapper clientMapper,  AppUserService userService) {
+        this.entityManager = entityManager;
+        this.criteriaBuilder = entityManager.getCriteriaBuilder();
+        this.clientMapper = clientMapper;
+        this.userService = userService;
+    }
+
+    public List<ClientDto> findAllWithFilters(ClientSearchCriteria clientSearchCriteria, Principal principal) {
+
+        CriteriaQuery<Client> criteriaQuery = criteriaBuilder.createQuery(Client.class);
+        Root<Client> ordersRoot = criteriaQuery.from(Client.class);
+        Predicate predicate = getPredicate(clientSearchCriteria, ordersRoot, principal);
+        criteriaQuery.where(predicate);
+
+        TypedQuery<Client> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Client> orders = typedQuery.getResultList();
+
+        return orders.stream().map(clientMapper::toDto).collect(Collectors.toList());
+    }
+
+    private Predicate getPredicate(ClientSearchCriteria clientSearchCriteria, Root<Client> clientRoot, Principal principal) {
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (Objects.nonNull(clientSearchCriteria.getName())) {
+            predicates.add(criteriaBuilder.like(clientRoot.get("name"), "%" + clientSearchCriteria.getName() + "%"));
+        }
+
+        AppUser user = userService.findByUsername(principal.getName());
+        Boolean isCloudAdmin = user.getRoles().contains(Role.CLOUD_ADMIN);
+
+        if (!isCloudAdmin) {
+            Optional<Employment> takingEmployment = user.getEmployments().stream()
+                    .filter(employment -> employment.getDateOfDismiss() == null)
+                    .findFirst();
+
+            if (takingEmployment.isPresent()) {
+                Long companyId = takingEmployment.get().getCompany().getId();
+                predicates.add(criteriaBuilder.equal(clientRoot.get("company").get("id"), companyId));
+                return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+            } else {
+                throw new IllegalArgumentException("The logged user is not a registered employee");
+            }
+        } else {
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        }
+    }
+}
