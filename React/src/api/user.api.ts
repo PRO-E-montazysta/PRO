7c577691c69cc638d9606d74823bd88d4a410c90
Natalia Sawicka <81017505+s18288@users.noreply.github.com>
2023-03-24 20:37:53 +0100

    +UserDetails

--- React/src/api/user.api.ts
+++ React/src/api/user.api.ts
@@ -1,13 +1,34 @@
 import { makeServiceCall, PayloadProps } from './utils.api'
+import { AppUser } from '../types/model/AppUser'
 
 export const getFilteredUsers = (payload: PayloadProps) => {
-    return makeServiceCall('/users/filter', 'GET', { ...payload })
+  return makeServiceCall('/users/filter', 'GET', { ...payload })
+}
+
+export const getAllUsers = () => {
+  return makeServiceCall('/users/all', 'GET', {})
 }
 
 export const getUserById = (id: string) => {
-    return makeServiceCall(`/users/${id}`, 'GET', {})
+  return makeServiceCall(`/users/${id}`, 'GET', {})
 }
 
 export const getAboutMeInfo = () => {
-    return makeServiceCall(`/aboutme`, 'GET', {})
+  return makeServiceCall(`/aboutme`, 'GET', {})
+}
+
+export const getUserDetails = (id: string) => {
+  return makeServiceCall(`/users/${id}`, 'GET', {})
+}
+
+export const updateUser = (data: AppUser) => {
+  return makeServiceCall(`/users/${data.id}`, 'PUT', { body: data })
+}
+
+export const postUser = (data: AppUser) => {
+  return makeServiceCall(`/users`, 'POST', { body: data })
+}
+
+export const deleteUser = (id: string | number) => {
+  return makeServiceCall(`/users/${id}`, 'DELETE', {})
 }
