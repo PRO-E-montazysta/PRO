2d10c40e6758d309e275c45085d0f173189a3d89
Krzysztof Sidor <ksidor93@gmail.com>
2023-03-25 12:55:19 +0100

    toolList implementation

---
+++ Android/app/src/main/java/com/example/e_montaysta/data/datasource/ToolsDataSource.kt
@@ -0,0 +1,12 @@
+package com.example.e_montaysta.data.datasource
+
+import com.example.e_montaysta.data.model.Tool
+
+class ToolsDataSource {
+
+    companion object {
+        val toolList = listOf<Tool>(
+            Tool("Młotek", 1, "Młotki")
+        )
+    }
+}
---
+++ Android/app/src/main/java/com/example/e_montaysta/data/model/Tool.kt
@@ -0,0 +1,10 @@
+package com.example.e_montaysta.data.model
+
+/**
+ * Data class that captures tool information
+ */
+data class Tool (
+    val name: String,
+    val id: Int,
+    val category: String
+)
