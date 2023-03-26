b6e3adf7dffb3b03c9478f1b82aa8609458d6b17
Kamil Kuryłek <46301983+kurylek@users.noreply.github.com>
2023-03-26 21:22:29 +0200

    CU-862jgbv7b_Change-enum-in-events (#124)
    * Move OrderStatus
    
    * Create EventStatus.java
    
    * Change model and dto
    
    * Change seeding

---
+++ Java/E-montazysta/src/main/java/com/emontazysta/enums/EventStatus.java
@@ -0,0 +1,10 @@
+package com.emontazysta.enums;
+
+public enum EventStatus {
+    CREATED,
+    IN_PROGRESS,
+    REPAIRED,
+    ELIMINATED,
+    MISSING,
+    OTHER
+}
--- Java/E-montazysta/src/main/java/com/emontazysta/model/OrderStage.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/model/OrderStage.java
@@ -1,5 +1,6 @@
 package com.emontazysta.model;
 
+import com.emontazysta.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
