6311523aead55b4ca3fa52193cc786957a043c17
Kamil Kuryłek <kurylek98@gmail.com>
2023-03-28 21:37:56 +0200

    Add filtering by eventType and eventDate

--- Java/E-montazysta/src/main/java/com/emontazysta/repository/criteria/EventCriteriaRepository.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/repository/criteria/EventCriteriaRepository.java
@@ -9,11 +9,9 @@ import com.emontazysta.model.searchcriteria.EventSearchCriteria;
 import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import java.security.Principal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -60,6 +58,7 @@ public class EventCriteriaRepository {
 
     private Predicate getToolEventPredicate(EventSearchCriteria eventSearchCriteria, Root<ToolEvent> toolEventRoot, Principal principal) {
         List<Predicate> predicates = new ArrayList<>();
+
         if (Objects.nonNull(eventSearchCriteria.getItemName())) {
             predicates.add(criteriaBuilder.like(toolEventRoot.get("tool").get("name"), "%" + eventSearchCriteria.getItemName() + "%"));
         }
@@ -72,11 +71,44 @@ public class EventCriteriaRepository {
             predicates.add(criteriaBuilder.or(typeOfStatusPredicates.toArray(new Predicate[0])));
         }
 
+        if (Objects.nonNull(eventSearchCriteria.getEventType())) {
+            List<Predicate> eventTypePredicates = new ArrayList<>();
+            for (String type : eventSearchCriteria.getEventType()) {
+                eventTypePredicates.add(criteriaBuilder.equal(criteriaBuilder.substring(toolEventRoot.get("tool").get("code"), 0, 4), type));
+            }
+            predicates.add(criteriaBuilder.or(eventTypePredicates.toArray(new Predicate[0])));
+        }
+
+        if (Objects.nonNull(eventSearchCriteria.getEventDateMin())
+                && Objects.nonNull(eventSearchCriteria.getEventDateMax())) {
+
+            if (LocalDateTime.parse(eventSearchCriteria.getEventDateMin())
+                    .isBefore(LocalDateTime.parse(eventSearchCriteria.getEventDateMax())) ||
+                    LocalDateTime.parse(eventSearchCriteria.getEventDateMin())
+                            .isEqual(LocalDateTime.parse(eventSearchCriteria.getEventDateMax()))) {
+
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(toolEventRoot.get("eventDate"),
+                        LocalDateTime.parse(eventSearchCriteria.getEventDateMin())));
+
+                predicates.add(criteriaBuilder.lessThanOrEqualTo(toolEventRoot.get("eventDate"),
+                        LocalDateTime.parse(eventSearchCriteria.getEventDateMax())));
+            } else {
+                throw new IllegalArgumentException("The start time must precede the end time");
+            }
+        } else if (Objects.nonNull(eventSearchCriteria.getEventDateMin())) {
+            predicates.add(criteriaBuilder.greaterThanOrEqualTo(toolEventRoot.get("eventDate"),
+                    LocalDateTime.parse(eventSearchCriteria.getEventDateMin())));
+        } else if (Objects.nonNull(eventSearchCriteria.getEventDateMax())) {
+            predicates.add(criteriaBuilder.lessThanOrEqualTo(toolEventRoot.get("eventDate"),
+                    LocalDateTime.parse(eventSearchCriteria.getEventDateMax())));
+        }
+
         return  criteriaBuilder.and(predicates.toArray(new Predicate[0]));
     }
 
     private Predicate getElementEventPredicate(EventSearchCriteria eventSearchCriteria, Root<ElementEvent> elementEventRoot, Principal principal) {
         List<Predicate> predicates = new ArrayList<>();
+
         if (Objects.nonNull(eventSearchCriteria.getItemName())) {
             predicates.add(criteriaBuilder.like(elementEventRoot.get("element").get("name"), "%" + eventSearchCriteria.getItemName() + "%"));
         }
@@ -87,6 +119,38 @@ public class EventCriteriaRepository {
                 typeOfStatusPredicates.add(criteriaBuilder.equal(elementEventRoot.get("status"), EventStatus.valueOf(type)));
             }
             predicates.add(criteriaBuilder.or(typeOfStatusPredicates.toArray(new Predicate[0])));
+        }
+
+        if (Objects.nonNull(eventSearchCriteria.getEventType())) {
+            List<Predicate> eventTypePredicates = new ArrayList<>();
+            for (String type : eventSearchCriteria.getEventType()) {
+                eventTypePredicates.add(criteriaBuilder.equal(criteriaBuilder.substring(elementEventRoot.get("element").get("code"), 0, 7), type));
+            }
+            predicates.add(criteriaBuilder.or(eventTypePredicates.toArray(new Predicate[0])));
+        }
+
+        if (Objects.nonNull(eventSearchCriteria.getEventDateMin())
+                && Objects.nonNull(eventSearchCriteria.getEventDateMax())) {
+
+            if (LocalDateTime.parse(eventSearchCriteria.getEventDateMin())
+                    .isBefore(LocalDateTime.parse(eventSearchCriteria.getEventDateMax())) ||
+                    LocalDateTime.parse(eventSearchCriteria.getEventDateMin())
+                            .isEqual(LocalDateTime.parse(eventSearchCriteria.getEventDateMax()))) {
+
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(elementEventRoot.get("eventDate"),
+                        LocalDateTime.parse(eventSearchCriteria.getEventDateMin())));
+
+                predicates.add(criteriaBuilder.lessThanOrEqualTo(elementEventRoot.get("eventDate"),
+                        LocalDateTime.parse(eventSearchCriteria.getEventDateMax())));
+            } else {
+                throw new IllegalArgumentException("The start time must precede the end time");
+            }
+        } else if (Objects.nonNull(eventSearchCriteria.getEventDateMin())) {
+            predicates.add(criteriaBuilder.greaterThanOrEqualTo(elementEventRoot.get("eventDate"),
+                    LocalDateTime.parse(eventSearchCriteria.getEventDateMin())));
+        } else if (Objects.nonNull(eventSearchCriteria.getEventDateMax())) {
+            predicates.add(criteriaBuilder.lessThanOrEqualTo(elementEventRoot.get("eventDate"),
+                    LocalDateTime.parse(eventSearchCriteria.getEventDateMax())));
         }
 
         return  criteriaBuilder.and(predicates.toArray(new Predicate[0]));
--- Java/E-montazysta/src/main/java/com/emontazysta/service/impl/ToolServiceImpl.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/service/impl/ToolServiceImpl.java
@@ -54,7 +54,7 @@ public class ToolServiceImpl implements ToolService {
     public ToolDto add(ToolDto toolDto) {
         Tool tool = toolMapper.toEntity(toolDto);
         tool.setCreatedAt(LocalDate.now());
-        tool.setCode(UUID.randomUUID().toString());
+        tool.setCode("TOOL|"+UUID.randomUUID());
         return toolMapper.toDto(repository.save(tool));
     }
 
