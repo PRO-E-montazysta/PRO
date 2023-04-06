f94e89ea06b71f9721cfe717dd29095fc60f7e68
Marcin H <79579871+Marcin-H@users.noreply.github.com>
2023-04-06 14:39:32 +0200

    CU-862jjde2r_Add-company-admin-data-to-form-while-creating-new-company (#142)
    * feat: add company admin form to new company form
    
    * feat: admin form valdidation, expandMore component fix
    
    * fix:update company
    
    * Added companyStatus and createdAt
    
    * Remove console.log
    
    ---------
    
    Co-authored-by: Kamil Kuryłek <kurylek98@gmail.com>

---
+++ React/src/components/expandMore/ExpandMore.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react'
+import { styled } from '@mui/material/styles'
+import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
+import Collapse from '@mui/material/Collapse'
+import IconButton, { IconButtonProps } from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
+interface CustomExpandMoreProps extends IconButtonProps {
+    expand: boolean
+}
+
+const CustomExpandMore = styled((props: CustomExpandMoreProps) => {
+    const { expand, ...other } = props
+    return <IconButton {...other} />
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}))
+
+type ExpandMoreProps = {
+    titleIcon: JSX.Element
+    title: string
+    cardContent: JSX.Element
+}
+
+const ExpandMore = ({ titleIcon, title, cardContent }: ExpandMoreProps) => {
+    const [expandedInformation, setExpandedInformation] = useState(false)
+
+    const handleExpandInformationClick = () => {
+        setExpandedInformation(!expandedInformation)
+    }
+
+    return (
+        <>
+            <CardActions disableSpacing>
+                <IconButton aria-label="share">{titleIcon}</IconButton>
+                <Typography variant="body2" color="text.secondary">
+                    {title}
+                </Typography>
+                <CustomExpandMore
+                    expand={expandedInformation}
+                    onClick={handleExpandInformationClick}
+                    aria-expanded={expandedInformation}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </CustomExpandMore>
+            </CardActions>
+            <Collapse in={expandedInformation} timeout="auto" unmountOnExit>
+                <CardContent>{cardContent}</CardContent>
+            </Collapse>
+        </>
+    )
+}
+
+export default ExpandMore
