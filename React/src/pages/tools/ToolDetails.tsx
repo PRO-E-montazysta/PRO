dfdcbe3c3f49d99e24d8c9b6baf8e5a858765e6e
Kamil Kuryłek <kurylek98@gmail.com>
2023-04-19 10:10:14 +0200

    Fresh tool view

--- React/src/pages/tools/ToolDetails.tsx
+++ React/src/pages/tools/ToolDetails.tsx
@@ -1,326 +1,115 @@
-import { Button, CircularProgress, Divider, Grid, Paper, TextField, Typography } from '@mui/material'
-import { Box } from '@mui/system'
-import { AxiosError } from 'axios'
 import { useFormik } from 'formik'
-import { useEffect, useState } from 'react'
-import { useMutation, useQuery } from 'react-query'
-import { useNavigate, useParams } from 'react-router-dom'
-import { deleteTool, getToolDetails, postTool, updateTool } from '../../api/tool.api'
-import { theme } from '../../themes/baseTheme'
-
-import SaveIcon from '@mui/icons-material/Save'
-import ReplayIcon from '@mui/icons-material/Replay'
-import CloseIcon from '@mui/icons-material/Close'
-import EditIcon from '@mui/icons-material/Edit'
-import DeleteIcon from '@mui/icons-material/Delete'
-import { emptyForm, validationSchema } from './helper'
-import FormInput from '../../components/form/FormInput'
-import FormLabel from '../../components/form/FormLabel'
-import FormSelect from '../../components/form/FormSelect'
-import DialogInfo, { DialogInfoParams } from '../../components/dialogInfo/DialogInfo'
-import { Tool } from '../../types/model/Tool'
-import { Warehouse } from '../../types/model/Warehouse'
-import { getAllWarehouses } from '../../api/warehouse.api'
-import { formatArrayToOptions } from '../../helpers/format.helper'
-import { ToolType } from '../../types/model/ToolType'
-import { getAllToolTypes } from '../../api/toolType.api'
-import PrintQRCodeLabel from '../../components/label/PrintQRCodeLabel'
+import { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useFormStructure } from './helper'
+import { DialogGlobalContext } from '../../providers/DialogGlobalProvider'
+import { getInitValues, getValidatinSchema } from '../../helpers/form.helper'
+import { useAddTool, useToolData, useDeleteTool, useEditTool } from './hooks'
+import { useQueriesStatus } from '../../hooks/useQueriesStatus'
+import FormBox from '../../components/form/FormBox'
+import FormTitle from '../../components/form/FormTitle'
+import FormPaper from '../../components/form/FormPaper'
+import QueryBoxStatus from '../../components/base/QueryStatusBox'
+import { FormStructure } from '../../components/form/FormStructure'
+import { FormButtons } from '../../components/form/FormButtons'
+import { PageMode } from '../../types/form'
 
 const ToolDetails = () => {
     const params = useParams()
-    const [readonlyMode, setReadonlyMode] = useState(true)
-    const [initData, setInitData] = useState(emptyForm)
-    const navigation = useNavigate()
+    const [pageMode, setPageMode] = useState<PageMode>('read')
+    const { showDialog } = useContext(DialogGlobalContext)
+    const formStructure = useFormStructure()
+    const [initData, setInitData] = useState(getInitValues(formStructure))
 
-    const [dialog, setDialog] = useState({
-        open: false,
-        dialogText: [''],
-        confirmAction: () => {},
-        confirmLabel: '',
-    })
+    //mutations and queries
+    const addToolMutation = useAddTool()
+    const editToolMutation = useEditTool((data) => handleOnEditSuccess(data))
+    const deleteToolMutation = useDeleteTool(() => toolData.remove())
+    const toolData = useToolData(params.id)
+    //status for all mutations and queries
+    const queriesStatus = useQueriesStatus([toolData], [addToolMutation, editToolMutation, deleteToolMutation])
 
-    const handleSubmit = () => {
-        if (params.id == 'new') mutationPost.mutate(JSON.parse(JSON.stringify(formik.values)))
-        else mutationUpdate.mutate(JSON.parse(JSON.stringify(formik.values)))
+    const handleSubmit = (values: any) => {
+        if (pageMode == 'new') addToolMutation.mutate(values)
+        else if (pageMode == 'edit') editToolMutation.mutate(values)
+        else console.warn('Try to submit while read mode')
     }
 
     const handleDelete = () => {
-        displayInfo({
-            dialogText: ['Czy na pewno chcesz usunąć firmę?'],
-            confirmAction: () => {
-                setDialog({ ...dialog, open: false })
-                if (formik.values.id) mutationDelete.mutate(formik.values.id)
+        showDialog({
+            title: 'Czy na pewno chcesz usunąć nardzędzie?',
+            btnOptions: [
+                { text: 'Usuń', value: 1, variant: 'contained' },
+                { text: 'Anuluj', value: 0, variant: 'outlined' },
+            ],
+            callback: (result: number) => {
+                if (result == 1 && params.id && Number.isInteger(params.id)) deleteToolMutation.mutate(params.id)
             },
-            confirmLabel: 'Usuń',
-            cancelAction: () => {
-                setDialog({ ...dialog, open: false })
-            },
-            cancelLabel: 'Anuluj',
         })
     }
 
-    const mutationPost = useMutation({
-        mutationFn: postTool,
-        onSuccess(data) {
-            displayInfo({
-                dialogText: ['Nowe narzędzie utworzone pomyślnie'],
-                confirmAction: () => {
-                    setDialog({ ...dialog, open: false })
-                    if (data.id) navigation(`/tools/${data.id}`)
-                    else navigation(`/tools`)
-                },
-                confirmLabel: 'Ok',
-            })
-        },
-        onError(error: Error) {
-            displayInfo({
-                dialogText: ['Błąd poczas tworzenia nowego narzędzia', error.message],
-                confirmAction: () => {
-                    setDialog({ ...dialog, open: false })
-                },
-                confirmLabel: 'Ok',
-            })
-        },
-    })
-
-    const mutationUpdate = useMutation({
-        mutationFn: updateTool,
-        onSuccess(data) {
-            queryData.refetch({
-                queryKey: ['tool', { id: data.id }],
-            })
-            setReadonlyMode(true)
-            displayInfo({
-                dialogText: ['Zmiany w narzędziu zostały zapisane'],
-                confirmAction: () => {
-                    setDialog({ ...dialog, open: false })
-                },
-                confirmLabel: 'Ok',
-            })
-        },
-        onError(error: Error) {
-            displayInfo({
-                dialogText: ['Błąd poczas zapisywania narzędzia', error.message],
-                confirmAction: () => {
-                    setDialog({ ...dialog, open: false })
-                },
-                confirmLabel: 'Ok',
-            })
-        },
-    })
-
-    const mutationDelete = useMutation({
-        mutationFn: deleteTool,
-        onSuccess(data) {
-            displayInfo({
-                dialogText: ['Narzędzie zostało usunięte'],
-                confirmAction: () => {
-                    setDialog({ ...dialog, open: false })
-                    queryData.remove()
-                    navigation('/tools')
-                },
-                confirmLabel: 'Ok',
-            })
-        },
-        onError(error: Error) {
-            displayInfo({
-                dialogText: ['Błąd poczas usuwania narzędzia', error.message],
-                confirmAction: () => {
-                    setDialog({ ...dialog, open: false })
-                },
-                confirmLabel: 'Ok',
-            })
-        },
-    })
-
-    const queryData = useQuery<Tool, AxiosError>(
-        ['tool', { id: params.id }],
-        async () => getToolDetails(params.id && params.id != 'new' ? params.id : ''),
-        {
-            enabled: !!params.id && params.id != 'new',
-        },
-    )
-
-    const queryWarehouse = useQuery<Array<Warehouse>, AxiosError>(['warehouse-list'], getAllWarehouses)
-    const queryToolType = useQuery<Array<ToolType>, AxiosError>(['toolType-list'], getAllToolTypes)
-
     const formik = useFormik({
         initialValues: initData,
-        validationSchema: validationSchema,
+        validationSchema: getValidatinSchema(formStructure, pageMode),
         onSubmit: handleSubmit,
     })
 
     const handleReset = () => {
         formik.resetForm()
-        formik.setValues(JSON.parse(JSON.stringify(initData)))
+        formik.setValues(initData)
     }
 
     const handleCancel = () => {
         handleReset()
-        setReadonlyMode(true)
+        setPageMode('read')
+    }
+
+    const handleOnEditSuccess = (data: any) => {
+        toolData.refetch({
+            queryKey: ['tool', { id: data.id }],
+        })
+        setPageMode('read')
     }
 
     useEffect(() => {
-        if (queryData.data) {
-            formik.setValues(JSON.parse(JSON.stringify(queryData.data)))
-            setInitData(JSON.parse(JSON.stringify(queryData.data)))
+        if (toolData.data) {
+            formik.setValues(toolData.data)
+            setInitData(toolData.data)
         }
-    }, [queryData.data])
+    }, [toolData.data])
 
     useEffect(() => {
         if (params.id == 'new') {
-            setReadonlyMode(false)
-            formik.setValues(JSON.parse(JSON.stringify(emptyForm)))
-            setInitData(JSON.parse(JSON.stringify(emptyForm)))
-        } else setReadonlyMode(true)
+            setPageMode('new')
+            formik.setValues(getInitValues(formStructure))
+            setInitData(getInitValues(formStructure))
+        } else {
+            setPageMode('read')
+        }
     }, [params.id])
 
-    const displayInfo = (params: DialogInfoParams) => {
-        setDialog({
-            ...params,
-            open: true,
-        })
-    }
-
     return (
-        <>
-            <Box maxWidth={800} style={{ margin: 'auto', marginTop: '20px' }}>
-                <Paper sx={{ p: '10px' }}>
-                    {queryData.isLoading || queryData.isError ? (
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                minHeight: '200px',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                            }}
-                        >
-                            {queryData.isLoading ? (
-                                <CircularProgress />
-                            ) : (
-                                <Typography>Nie znaleziono narzędzia</Typography>
-                            )}
-                        </Box>
-                    ) : (
-                        <>
-                            <Grid container>
-                                <Grid
-                                    item
-                                    xs={6}
-                                    style={{
-                                        textAlign: 'end',
-                                    }}
-                                >
-                                    <FormLabel label="Nazwa narzędzia" formik={formik} id={'name'} />
-                                    <FormLabel label="Data utworzenia" formik={formik} id={'createdAt'} />
-                                    <FormLabel label="Kod" formik={formik} id={'code'} />
-                                    <FormLabel label="Magazyn" formik={formik} id={'warehouseId'} />
-                                    <FormLabel label="Typ narzędzia" formik={formik} id={'toolTypeId'} />
-                                </Grid>
-                                <Divider
-                                    orientation="vertical"
-                                    variant="middle"
-                                    style={{ borderColor: theme.palette.primary.main }}
-                                    sx={{ mr: '-1px' }}
-                                    flexItem
-                                />
-                                <Grid item xs={6}>
-                                    <FormInput id={'name'} formik={formik} readonly={readonlyMode} />
-                                    <FormInput
-                                        id={'createdAt'}
-                                        formik={formik}
-                                        readonly
-                                        style={{ marginTop: !readonlyMode ? '12px' : '' }}
-                                        type="datetime-local"
-                                    />
-                                    <FormInput id={'code'} formik={formik} readonly />
-                                    <FormSelect
-                                        id={'warehouseId'}
-                                        formik={formik}
-                                        readonly={readonlyMode}
-                                        options={formatArrayToOptions(
-                                            'id',
-                                            (x: Warehouse) => x.name,
-                                            queryWarehouse.data,
-                                        )}
-                                    />
-                                    <FormSelect
-                                        id={'toolTypeId'}
-                                        formik={formik}
-                                        readonly={readonlyMode}
-                                        options={formatArrayToOptions(
-                                            'id',
-                                            (x: ToolType) => x.name,
-                                            queryToolType.data,
-                                        )}
-                                    />
-                                </Grid>
-                            </Grid>
-                            <Box sx={{ margin: '20px', gap: '20px', display: 'flex', flexDirection: 'row-reverse' }}>
-                                {readonlyMode && params.id != 'new' ? (
-                                    <>
-                                        <PrintQRCodeLabel label={queryData.data?.name!} code={queryData.data?.code!} />
-                                        <Button
-                                            color="primary"
-                                            startIcon={<EditIcon />}
-                                            variant="contained"
-                                            type="submit"
-                                            style={{ width: 120 }}
-                                            onClick={() => setReadonlyMode(false)}
-                                        >
-                                            Edytuj
-                                        </Button>
-                                        <Button
-                                            color="error"
-                                            startIcon={<DeleteIcon />}
-                                            variant="contained"
-                                            type="submit"
-                                            style={{ width: 120 }}
-                                            onClick={handleDelete}
-                                        >
-                                            Usuń
-                                        </Button>
-                                    </>
-                                ) : (
-                                    <>
-                                        <Button
-                                            color="primary"
-                                            startIcon={<SaveIcon />}
-                                            variant="contained"
-                                            onClick={formik.submitForm}
-                                            style={{ width: 120 }}
-                                        >
-                                            Zapisz
-                                        </Button>
-                                        <Button
-                                            color="primary"
-                                            startIcon={<ReplayIcon style={{ transform: 'rotate(-0.25turn)' }} />}
-                                            style={{ color: theme.palette.primary.main, width: 120 }}
-                                            variant="outlined"
-                                            onClick={handleReset}
-                                        >
-                                            Reset
-                                        </Button>
-                                        {params.id != 'new' && (
-                                            <Button
-                                                color="primary"
-                                                startIcon={<CloseIcon style={{ transform: 'rotate(-0.25turn)' }} />}
-                                                style={{ color: theme.palette.primary.main, width: 120 }}
-                                                variant="outlined"
-                                                onClick={handleCancel}
-                                            >
-                                                Anuluj
-                                            </Button>
-                                        )}
-                                    </>
-                                )}
-                            </Box>
-                        </>
-                    )}
-                </Paper>
-            </Box>
-            {dialog.open && <DialogInfo {...dialog} />}
-        </>
+        <FormBox>
+            <FormTitle text="Narzędzie" />
+            <FormPaper>
+                {queriesStatus.result != 'isSuccess' ? (
+                    <QueryBoxStatus queriesStatus={queriesStatus} />
+                ) : (
+                    <>
+                        <FormStructure formStructure={formStructure} formik={formik} pageMode={pageMode} />
+                        <FormButtons
+                            id={params.id}
+                            onCancel={handleCancel}
+                            onDelete={handleDelete}
+                            onEdit={() => setPageMode('edit')}
+                            onReset={handleReset}
+                            onSubmit={formik.submitForm}
+                            readonlyMode={pageMode == 'read'}
+                        />
+                    </>
+                )}
+            </FormPaper>
+        </FormBox>
     )
 }
 
