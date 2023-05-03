78ffe4832285d71704d42b76febe72cd5c066e0f
Kamil Kuryłek <kurylek98@gmail.com>
2023-05-03 18:50:34 +0200

    Add some OrderStageStatuses

--- Java/E-montazysta/src/main/java/com/emontazysta/enums/OrderStageStatus.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/enums/OrderStageStatus.java
@@ -5,7 +5,9 @@ public enum OrderStageStatus {
     PLANNING, //Planowanie | Etap utworzony przez specjalistę | Automatycznie po stworzeniu
     ADDING_FITTERS, //Przypisywanie | Brygadzista przypisuje montażystów | Specjalista
     PICK_UP, //Odbieranie | Magazynier wydaje elementy i narzędzia | Brygadzista
-    REALESED, //Wydano | Magazynier wydał narzędzia | Magazynier
+    REALESED, //Wydano | Magazynier wydał narzędzia i elementy | Magazynier
     ON_WORK, //W realizacji | Brygadzista zaakceptował wydanie i rozpoczął prace | Brygadzista
+    RETURN, //Zwrot | Brygadzista zwraca narzędzia i elementy | Brygadzista
+    RETURNED, //Zwrócono | Brygadzista zwrócił wszystkie narzędzia | Magazynier
     FINISHED, //Zakończono | Zakończono etap | Brygadzista
 }
