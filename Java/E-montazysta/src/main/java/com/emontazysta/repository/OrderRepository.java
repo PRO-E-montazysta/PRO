e29af8ae9da580fffb24bd4dc5de223a7f7fc552
Kamil Kuryłek <kurylek98@gmail.com>
2023-05-08 21:42:15 +0200

    Remove method- its provided by default

--- Java/E-montazysta/src/main/java/com/emontazysta/repository/OrderRepository.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/repository/OrderRepository.java
@@ -3,11 +3,5 @@ package com.emontazysta.repository;
 import com.emontazysta.model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-
 public interface OrderRepository extends JpaRepository<Orders, Long> {
-
-    public Optional<Orders> findById(Long id);
-
 }
