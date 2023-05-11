92c5b66194246cf763bd70ae029b8ec17c06583c
Kamil Kuryłek <kurylek98@gmail.com>
2023-05-11 11:59:02 +0200

    Update repo

--- Java/E-montazysta/src/main/java/com/emontazysta/repository/NotificationRepository.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/repository/NotificationRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-    List<Notification> getAllByNotifiedEmployeesContaining(AppUser notifiedEmployee);
+    List<Notification> getAllByNotifiedEmployee(AppUser notifiedEmployee);
 
 
 }
