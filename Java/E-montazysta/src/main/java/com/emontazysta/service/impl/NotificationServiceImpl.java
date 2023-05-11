d32393168a8cc5aeae17be826237b78e05c41de6
Kamil Kuryłek <kurylek98@gmail.com>
2023-05-11 11:59:14 +0200

    Update servie implementation

--- Java/E-montazysta/src/main/java/com/emontazysta/service/impl/NotificationServiceImpl.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/service/impl/NotificationServiceImpl.java
@@ -4,12 +4,10 @@ import com.emontazysta.enums.NotificationType;
 import com.emontazysta.mapper.NotificationMapper;
 import com.emontazysta.model.*;
 import com.emontazysta.model.dto.NotificationDto;
-import com.emontazysta.repository.AppUserRepository;
 import com.emontazysta.repository.NotificationRepository;
 import com.emontazysta.repository.OrderRepository;
 import com.emontazysta.repository.OrderStageRepository;
 import com.emontazysta.service.NotificationService;
-import com.emontazysta.service.OrderStageService;
 import com.emontazysta.util.AuthUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -34,40 +32,52 @@ public class NotificationServiceImpl implements NotificationService {
 
     @Override
     public void createNotification(List<AppUser> notifiedEmployees, Long triggerId, NotificationType triggerType) {
-        Notification notification = Notification.builder()
-                .createdAt(LocalDateTime.now())
-                .notificationType(triggerType)
-                .content(triggerType.getMessage())
-                .createdBy(authUtils.getLoggedUser())
-                .notifiedEmployees(notifiedEmployees)
-                .build();
+        for(AppUser appUser : notifiedEmployees) {
+            Notification notification = Notification.builder()
+                    .createdAt(LocalDateTime.now())
+                    .notificationType(triggerType)
+                    .content(triggerType.getMessage())
+                    .notifiedEmployee(appUser)
+                    .createdBy(authUtils.getLoggedUser())
+                    .build();
 
-        switch (triggerType) {
-            case ORDER_CREATED:
-            case ACCEPT_ORDER:
-            case FOREMAN_ASSIGNMENT:
-                Orders order = orderRepository.findById(triggerId).orElseThrow(EntityNotFoundException::new);
-                notification.setOrder(order);
-                notification.setSubContent(order.getName());
-                break;
-            case FITTER_ASSIGNMENT:
-                OrderStage orderStage = orderStageRepository.findById(triggerId).orElseThrow(EntityNotFoundException::new);
-                notification.setOrderStage(orderStage);
-                notification.setSubContent(orderStage.getName());
-                break;
-            case AD_HOC_CREATED:
-                //TODO On DemandAdHoc implementation
-                break;
+            switch (triggerType) {
+                case ORDER_CREATED:
+                case ACCEPT_ORDER:
+                case FOREMAN_ASSIGNMENT:
+                    Orders order = orderRepository.findById(triggerId).orElseThrow(EntityNotFoundException::new);
+                    notification.setOrder(order);
+                    notification.setSubContent(order.getName());
+                    break;
+                case FITTER_ASSIGNMENT:
+                    OrderStage orderStage = orderStageRepository.findById(triggerId).orElseThrow(EntityNotFoundException::new);
+                    notification.setOrderStage(orderStage);
+                    notification.setSubContent(orderStage.getName());
+                    break;
+                case AD_HOC_CREATED:
+                    //TODO On DemandAdHoc implementation
+                    break;
+                /*case TOOL_EVENT:
+                    ToolEvent toolEvent = toolEventRepository.findById(triggerId).orElseThrow(EntityNotFoundException::new);
+                    notification.setToolEvent(toolEvent);
+                    notification.setSubContent("Usterka " + toolEvent.getTool().getName());
+                    break;
+                case ELEMENT_EVENT:
+                    ElementEvent elementEvent = elementEventRepository.findById(triggerId).orElseThrow(EntityNotFoundException::new);
+                    notification.setElementEvent(elementEvent);
+                    notification.setSubContent("Usterka " + elementEvent.getElement().getName());
+                    break;*/
+            }
+
+            notificationRepository.save(notification);
         }
-
-        notificationRepository.save(notification);
     }
 
     @Override
     public List<NotificationDto> getAllNotReaded(Long employeeId) {
         List<NotificationDto> notificationDtos = new ArrayList<>();
 
-        List<Notification> notifications = notificationRepository.getAllByNotifiedEmployeesContaining(appUserService.getById(employeeId));
+        List<Notification> notifications = notificationRepository.getAllByNotifiedEmployee(appUserService.getById(employeeId));
 
         if (!notifications.isEmpty()){
             notificationDtos = notifications.stream()
