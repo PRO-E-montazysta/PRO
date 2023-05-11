1a0698611ba1f7591af179ec9dfd278e625fe4b7
Kamil Kuryłek <kurylek98@gmail.com>
2023-05-11 10:08:15 +0200

    Display norification count

--- React/src/components/navbar/NotiButton.tsx
+++ React/src/components/navbar/NotiButton.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, IconButton } from '@mui/material'
+import { Box, IconButton } from '@mui/material'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import NotificationsIcon from '@mui/icons-material/Notifications'
@@ -6,6 +6,9 @@ import { Badge } from '@mui/material'
 import NotiMenu from './NotiMenu'
 import ClickAwayListener from '@mui/base/ClickAwayListener'
 import { UserInfo } from './NavActions'
+import { AxiosError } from 'axios'
+import { useQuery } from 'react-query'
+import { getNotifications } from '../../api/notification.api'
 
 type NotiButtonProps = {
     userInfo: UserInfo
@@ -26,6 +29,12 @@ const NotiButton = (props: NotiButtonProps) => {
         setOpen(false)
     }
 
+    const notificationQuery = useQuery<Array<Notification>, AxiosError>(['notifications'], async () =>
+        getNotifications(),
+    )
+
+    const notificationCount = notificationQuery.data ? Number(notificationQuery.data.length) : 0
+
     return (
         <>
             <ClickAwayListener onClickAway={handleClose}>
@@ -37,7 +46,7 @@ const NotiButton = (props: NotiButtonProps) => {
                         title="Powiadomienia"
                         sx={{ ml: '10px', width: 40, height: 40, border: '2px solid white' }}
                     >
-                        <Badge badgeContent={userInfo.notifications.length} color="error" sx={{ p: '3px' }}>
+                        <Badge badgeContent={notificationCount} color="error" sx={{ p: '3px' }}>
                             <NotificationsIcon />
                         </Badge>
                     </IconButton>
