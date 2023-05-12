fbdc840ad4695ffbb2169c1d02e6cee078786d07
Kamil Kuryłek <kurylek98@gmail.com>
2023-05-13 01:10:36 +0200

    Fix in constructor

--- Java/E-montazysta/src/main/java/com/emontazysta/model/Notification.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/model/Notification.java
@@ -23,13 +23,13 @@ import java.util.List;
 public class Notification {
 
     public Notification(Long id, String content, LocalDateTime createdAt, LocalDateTime readAt, AppUser createdBy,
-                        List<AppUser> notifiedEmployees, OrderStage orderStage) {
+                        AppUser notifiedEmployee, OrderStage orderStage) {
         this.id = id;
         this.content = content;
         this.createdAt = createdAt;
         this.readAt = readAt;
         this.createdBy = createdBy;
-        this.notifiedEmployees = notifiedEmployees;
+        this.notifiedEmployee = notifiedEmployee;
         this.orderStage = orderStage;
     }
 
