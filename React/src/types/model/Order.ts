4b7a97616fef94e706efd9cd66cbfc9c964f4d82
Kamil Kuryłek <kurylek98@gmail.com>
2023-05-12 11:53:30 +0200

    Update OrderStatus on front

--- React/src/types/model/Order.ts
+++ React/src/types/model/Order.ts
@@ -1,33 +1,34 @@
-
 export type Order = {
-    id: number,
-    name: string,
-    typeOfStatus: string,
-    plannedStart: Date,
-    plannedEnd: Date,
-    createdAt: Date,
-    editedAt: Date,
+    id: number
+    name: string
+    typeOfStatus: string
+    plannedStart: Date
+    plannedEnd: Date
+    createdAt: Date
+    editedAt: Date
     typeOfPriority: string
-    companyId: number,
-    managerId: number,
-    foremanId: number,
-    specialistId: number,
-    salesRepresentativeId: number,
-    locationId: number,
-    clientId: number,
+    companyId: number
+    managerId: number
+    foremanId: number
+    specialistId: number
+    salesRepresentativeId: number
+    locationId: number
+    clientId: number
     orderStages: Array<number>
     attachments: Array<number>
 }
 
-
 export const OrderStatus = {
-    PLANNED: "PLANOWANIE",
-    IN_PROGRESS: "W TRAKCIE",
-    FINISHED: "ZAKOŃCZONE"
+    CREATED: 'Utworzony',
+    PLANNING: 'Planowanie',
+    TO_ACCEPT: 'Do zaakceptowania',
+    ACCEPTED: 'Zaakceptowane',
+    IN_PROGRESS: 'W trakcie',
+    FINISHED: 'Zakończono',
 }
 
 export const OrderPriority = {
-    NORMAL: "NORMALNY",
-    IMPORTANT: "WAŻNY",
-    IMMEDIATE: "NATYCHMIASTOWY"
+    NORMAL: 'NORMALNY',
+    IMPORTANT: 'WAŻNY',
+    IMMEDIATE: 'NATYCHMIASTOWY',
 }
