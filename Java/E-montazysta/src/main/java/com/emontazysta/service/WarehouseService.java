f32446a60d49a116dad3778e3c2a31cb324524db
Kamil Kuryłek <46301983+kurylek@users.noreply.github.com>
2023-05-17 22:01:15 +0200

    CU-862ju0d3h_Split-Warehouse-and-Location-while-creating (#216)
    * Remove dto
    
    * Update after dto remove
    
    * Remove req. apartment number from location
    
    * Update toString in Location
    
    * Set Warehouse location on Location create

--- Java/E-montazysta/src/main/java/com/emontazysta/service/WarehouseService.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/service/WarehouseService.java
@@ -2,7 +2,6 @@ package com.emontazysta.service;
 
 import com.emontazysta.model.dto.WarehouseDto;
 import com.emontazysta.model.dto.WarehouseLocationDto;
-import com.emontazysta.model.dto.WarehouseWithLocationDto;
 import com.emontazysta.model.searchcriteria.WarehouseSearchCriteria;
 
 import java.util.List;
@@ -13,9 +12,8 @@ public interface WarehouseService {
     WarehouseDto getById(Long id);
     WarehouseDto add(WarehouseDto warehouse);
     void delete(Long id);
-    WarehouseDto update(Long id, WarehouseWithLocationDto warehouseWithLocationDto);
+    WarehouseDto update(Long id, WarehouseDto warehouseDto);
     List <WarehouseLocationDto> findAllWithFilters(WarehouseSearchCriteria warehouseSearchCriteria);
     WarehouseDto addWithWarehouseCount(WarehouseDto warehouse);
-    WarehouseDto addWarehouseWithLocation(WarehouseWithLocationDto warehouseWithLocationDto);
 
 }
