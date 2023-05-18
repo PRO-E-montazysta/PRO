6342044cce87b29fb86099551ac573eb648a283b
Krzysztof Sidor <ksidor93@gmail.com>
2023-05-18 14:49:45 +0200

    Frontend almost done

--- Android/app/src/main/java/com/example/e_montazysta/ui/release/ReleaseCreateFragment.kt
+++ Android/app/src/main/java/com/example/e_montazysta/ui/release/ReleaseCreateFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.e_montazysta.databinding.FragmentCreateReleaseBinding
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ReleaseCreateFragment : Fragment() {
@@ -20,6 +24,32 @@ class ReleaseCreateFragment : Fragment() {
         val stageId = args.stageId
         val binding: FragmentCreateReleaseBinding = FragmentCreateReleaseBinding.inflate(inflater, container, false)
         binding.viewModel = releaseCreateViewModel
+
+        val options = GmsBarcodeScannerOptions.Builder()
+            .setBarcodeFormats(
+                Barcode.FORMAT_QR_CODE,
+                Barcode.FORMAT_AZTEC)
+            .build()
+
+        val scanner = container?.let { GmsBarcodeScanning.getClient(it.context, options) }
+        binding.addObjectsToRelease.setOnClickListener{
+            scanner?.startScan()?.addOnSuccessListener {barcode ->
+                val code = barcode?.rawValue
+                when(code?.first()) {
+                    'E' -> binding.viewModel?.let { Toast.makeText(activity, it.addElementToRelease(code), Toast.LENGTH_LONG).show() }
+                    'T' -> binding.viewModel?.let { Toast.makeText(activity, it.addToolToRelease(code), Toast.LENGTH_LONG).show() }
+                    else -> {
+                        Toast.makeText(activity, "Niepoprawny kod QR!\nWartość: $code", Toast.LENGTH_LONG ).show()
+                    }
+                }
+            }?.addOnCanceledListener {
+
+            }?.addOnFailureListener { e ->
+                Toast.makeText(activity, e.message, Toast.LENGTH_LONG ).show()
+            }
+        }
+
+
         return binding.root
     }
 }
--- Android/app/src/main/java/com/example/e_montazysta/ui/release/ReleaseCreateViewModel.kt
+++ Android/app/src/main/java/com/example/e_montazysta/ui/release/ReleaseCreateViewModel.kt
@@ -1,11 +1,22 @@
 package com.example.e_montazysta.ui.release
 
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlin.coroutines.CoroutineContext
 
 class ReleaseCreateViewModel() : ViewModel(), CoroutineScope {
+
+
+
+    fun addElementToRelease(code: String?) : String?  {
+        return code
+    }
+    fun addToolToRelease(code: String?) : String? {
+        return code
+    }
+
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main
 }
