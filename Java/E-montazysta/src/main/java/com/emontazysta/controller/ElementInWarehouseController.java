07a2bbcfaf38cf0109f3096bba6528c1a09fcc5b
Kamil Kuryłek <kurylek98@gmail.com>
2023-05-21 20:19:35 +0200

    Add PreAuthorize to endpoints

--- Java/E-montazysta/src/main/java/com/emontazysta/controller/ElementInWarehouseController.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/controller/ElementInWarehouseController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -24,24 +25,28 @@ public class ElementInWarehouseController {
     private final ElementInWarehouseService service;
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('SCOPE_SPECIALIST', 'SCOPE_WAREHOUSE_MAN', 'SCOPE_WAREHOUSE_MANAGER')")
     @Operation(description = "Allows to get element in warehouse by given Id.", security = @SecurityRequirement(name = "bearer-key"))
     public ElementInWarehouseDto getElementInWarehouseById(@PathVariable Long id) {
         return service.getById(id);
     }
 
     @GetMapping("/element/{elementId}")
+    @PreAuthorize("hasAnyAuthority('SCOPE_SPECIALIST', 'SCOPE_WAREHOUSE_MAN', 'SCOPE_WAREHOUSE_MANAGER')")
     @Operation(description = "Allows to get element in warehouse by given Id.", security = @SecurityRequirement(name = "bearer-key"))
     public List<ElementInWarehouseFilterDto> getFilteredWarehouseCount(ElementInWarehouseSearchCriteria elementInWarehouseSearchCriteria, @PathVariable Long elementId) {
         return service.getFilteredWarehouseCount(elementId, elementInWarehouseSearchCriteria);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_WAREHOUSE_MANAGER')")
     @Operation(description = "Allows to delete element from warehouse by given Id.", security = @SecurityRequirement(name = "bearer-key"))
     public void deleteElementInWarehouseById(@PathVariable Long id) {
         service.delete(id);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('SCOPE_WAREHOUSE_MAN', 'SCOPE_WAREHOUSE_MANAGER')")
     @Operation(description = "Allows to update element from warehouse by given Id.", security = @SecurityRequirement(name = "bearer-key"))
     public ElementInWarehouseDto updateElementInWarehouse(@PathVariable Long id, @Valid @RequestBody ElementInWarehouseDto element) {
         return service.update(id, element);
