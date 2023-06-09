fb0c835bea4dd97ed8b8298636c19c1948e3e024
Kryhowsky <piter71-1997@o2.pl>
2023-06-09 11:28:48 +0200

    Improve file storage

--- Java/E-montazysta/src/main/java/com/emontazysta/repository/impl/FileSystemRepositoryImpl.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/repository/impl/FileSystemRepositoryImpl.java
@@ -1,22 +1,29 @@
 package com.emontazysta.repository.impl;
 
 import com.emontazysta.repository.FileSystemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Date;
+import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
 public class FileSystemRepositoryImpl implements FileSystemRepository {
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public String save(byte[] content, String fileName) throws IOException {
 
-        Path newFile = Paths.get(RESOURCES_DIR + new Date().getTime() + "-" + fileName);
+        String fileExtension = getFileExtension(fileName).orElseThrow();
+        String uniqueName = getUniqueFileName();
+        Path newFile = Paths.get(RESOURCES_DIR + uniqueName + "." + fileExtension).normalize();
         Files.createDirectories(newFile.getParent());
 
         //TODO UNCOMMENT IT TO SAVE FILES!
@@ -38,5 +45,20 @@ public class FileSystemRepositoryImpl implements FileSystemRepository {
     @Override
     public boolean delete(String location) throws IOException {
         return Files.deleteIfExists(Paths.get(location));
+    }
+
+    private String getUniqueFileName() {
+        String currentDate = String.valueOf(System.currentTimeMillis());
+        return cleanSpecialCharacters(bCryptPasswordEncoder.encode(currentDate));
+    }
+
+    private Optional<String> getFileExtension(String filename) {
+        return Optional.ofNullable(filename)
+                .filter(f -> f.contains("."))
+                .map(f -> f.substring(filename.lastIndexOf(".") + 1));
+    }
+
+    private String cleanSpecialCharacters(String fileName) {
+        return fileName.replaceAll("[\\\\/:*?\"<>|]", "");
     }
 }
