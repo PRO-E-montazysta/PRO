2dcc819509ca50db515d34e5953d923646bb11df
Krzysztof Maj <s18533@pjwstk.edu.pl>
2023-06-09 05:51:34 +0200

    added working settings screen with mobile navigation, theme selection and notifications toggle implemented

--- Android/app/src/main/java/com/example/e_montazysta/ui/activities/SettingsActivity.kt
+++ Android/app/src/main/java/com/example/e_montazysta/ui/activities/SettingsActivity.kt
@@ -1,33 +1,81 @@
 package com.example.e_montazysta.ui.activities
 
 import ThemeHelper
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.e_montazysta.R
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.fragment.app.Fragment
 import com.example.e_montazysta.databinding.ActivitySettingsBinding
+import com.google.android.material.switchmaterial.SwitchMaterial
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : Fragment() {
 
     private lateinit var binding: ActivitySettingsBinding
     private lateinit var themeHelper: ThemeHelper
+    private val notificationId = 1
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivitySettingsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = ActivitySettingsBinding.inflate(layoutInflater, container, false)
 
-        themeHelper = ThemeHelper(this)
+        themeHelper = ThemeHelper(requireContext())
+        binding.toolbar.title = "Ustawienia"
 
-        // Toolbar na górze
-        setSupportActionBar(binding.toolbar)
-        supportActionBar?.apply {
-            title = "Settings"
-            setDisplayHomeAsUpEnabled(true)
+        // Retrieve the reference to the SwitchMaterial widget
+        val notificationsSwitch: SwitchMaterial = binding.notificationsSwitch
+
+        // Add listener to handle switch state changes
+        notificationsSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // Enable notifications
+                // Create a notification channel
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    val channelId = "settingsChannel"
+                    val channelName = "Settings Notifications Channel"
+                    val channelDescription = "This is a notification channel within settings screen"
+
+                    val importance = NotificationManager.IMPORTANCE_DEFAULT
+                    val channel = NotificationChannel(channelId, channelName, importance).apply {
+                        description = channelDescription
+                    }
+
+                    // Register the channel with the system
+                    val notificationManager = requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                    notificationManager.createNotificationChannel(channel)
+                }
+
+                // Create and send a sample notification
+                val notificationBuilder = NotificationCompat.Builder(requireContext(), "settingsChannel")
+                    .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                    .setContentTitle("E-montażysta: Ustawienia")
+                    .setContentText("Włączono powiadomienia")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+                val notificationManager = NotificationManagerCompat.from(requireContext())
+                notificationManager.notify(notificationId, notificationBuilder.build())
+            } else {
+                // Cancel any existing notifications
+                val notificationManager = NotificationManagerCompat.from(requireContext())
+                notificationManager.cancel(notificationId)
+            }
         }
 
         // Get the saved theme preference
@@ -57,10 +105,6 @@ class SettingsActivity : AppCompatActivity() {
                 }
             }
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return true
+        return binding.root
     }
 }
