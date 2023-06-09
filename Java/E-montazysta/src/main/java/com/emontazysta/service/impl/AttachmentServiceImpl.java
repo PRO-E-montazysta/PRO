9c9633f2a0f58173a2a816a6fe2f660a0ad433b7
Kryhowsky <piter71-1997@o2.pl>
2023-06-09 11:34:08 +0200

    Serve NoSuchElementException

--- Java/E-montazysta/src/main/java/com/emontazysta/service/impl/AttachmentServiceImpl.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/service/impl/AttachmentServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.EntityNotFoundException;
 import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -45,6 +46,9 @@ public class AttachmentServiceImpl implements AttachmentService {
             Attachment attachment = attachmentMapper.toEntity(attachmentDto, file);
             attachment.setCreatedAt(LocalDateTime.now());
             return attachmentMapper.toDto(repository.save(attachment));
+        } catch (NoSuchFileException e) {
+            log.error("Error during saving attachment - filename does not contain correct extension", e);
+            return null;
         } catch (IOException e) {
             log.error("Error during saving attachment", e);
             return null;
