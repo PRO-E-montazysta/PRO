4842a00fab272db38072351f5ab11a69193f3335
Kamil Kuryłek <46301983+kurylek@users.noreply.github.com>
2023-06-30 14:10:39 +0200

    Remove password req

--- Java/E-montazysta/src/main/java/com/emontazysta/model/dto/CompanyWithAdminDto.java
+++ Java/E-montazysta/src/main/java/com/emontazysta/model/dto/CompanyWithAdminDto.java
@@ -34,8 +34,8 @@ public class CompanyWithAdminDto {
     @Email(message = "Email should be in valid format")
     @NotBlank(message = "Email cannot be empty")
     private String email;
-    @NotBlank(message = "Password cannot be empty")
-    @Length(min = 5, message = "Password must contain at least 5 characters" )
+    //@NotBlank(message = "Password cannot be empty")
+    //@Length(min = 5, message = "Password must contain at least 5 characters" )
     private String password;
     @NotBlank(message = "Username cannot be empty")
     @Length(min = 3, message = "Username must contain at least 3 characters" )
